c6586e2785c39b65fbd7daf6d2edc64733d07d29
pchchv <ipchchv@gmail.com>
2023-06-07 11:52:25 +0300

    implement ipv4StringCache and ipv6StringCache types

---
+++ groupingbase.go
@@ -0,0 +1,17 @@
+package goip
+
+type ipv4StringCache struct {
+	inetAtonOctalString,
+	inetAtonHexString *string
+}
+
+type ipv6StringCache struct {
+	normalizedIPv6String,
+	compressedIPv6String,
+	mixedString,
+	compressedWildcardString,
+	canonicalWildcardString,
+	networkPrefixLengthString,
+	base85String,
+	uncString *string
+}
