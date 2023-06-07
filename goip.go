d6550fa48663e98a9ce7e0b02bb1ba5ccf5bfb75
pchchv <ipchchv@gmail.com>
2023-06-07 11:25:21 +0300

    Implement an IsIPv4 and IsIPv6 methods

--- goip.go
+++ goip.go
@@ -1,4 +1,23 @@
 package goip
 
+import "strings"
+
+const (
+	// IPv4 represents Internet Protocol version 4
+	IPv4 IPVersion = "IPv4"
+	// IPv6 represents Internet Protocol version 6
+	IPv6 IPVersion = "IPv6"
+)
+
 // IPVersion is the version type used by IP address types.
 type IPVersion string
+
+// IsIPv4 returns true if this represents version 4
+func (version IPVersion) IsIPv4() bool {
+	return len(version) == 4 && strings.EqualFold(string(version), string(IPv4))
+}
+
+// IsIPv6 returns true if this represents version 6
+func (version IPVersion) IsIPv6() bool {
+	return len(version) == 4 && strings.EqualFold(string(version), string(IPv6))
+}
