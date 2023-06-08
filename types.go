63943d0873c8283fe649f09539c2e89b8ee0f5a5
pchchv <ipchchv@gmail.com>
2023-06-08 10:56:36 +0300

    Implement a PrefixBitCount and PrefixLen types

--- types.go
+++ types.go
@@ -6,3 +6,12 @@ package goip
 // so negative numbers of bits or numbers of bits greater than the address size are meaningless.
 // Using signed integers allows you to simplify arithmetic.
 type BitCount = int
+
+// PrefixBitCount is the number of bits in a non-zero PrefixLen.
+// For arithmetic you can use the signed integer type BitCount,
+// which you can get from PrefixLen using the Len method.
+type PrefixBitCount uint8
+
+// PrefixLen indicates the prefix length for an address, section, division group, segment or division.
+// A value of zero, i.e. nil, indicates that there is no prefix length.
+type PrefixLen = *PrefixBitCount
