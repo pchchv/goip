7219e4842e7511b99f51f0583f9aaa9764c943a9
pchchv <ipchchv@gmail.com>
2023-06-08 10:17:24 +0300

    Implement AddressValueError&AddressStringError types

--- address_error/error.go
+++ address_error/error.go
@@ -7,6 +7,21 @@ type AddressError interface {
 	GetKey() string
 }
 
+// AddressValueError occurs as a result of providing an invalid value for an address operation.
+// Used when the address or address component is too large or small,
+// when the prefix length is too large or small, or when prefixes in segments are inconsistent.
+// Not used when constructing new address components.
+// Not used when parsing strings to construct new address components,
+// in which case AddressStringError is used instead.
+type AddressValueError interface {
+	AddressError
+}
+
+// AddressStringError represents errors in address string formats used to identify addresses.
+type AddressStringError interface {
+	HostIdentifierError
+}
+
 // HostIdentifierError represents errors in string formats used to identify hosts.
 type HostIdentifierError interface {
 	AddressError
