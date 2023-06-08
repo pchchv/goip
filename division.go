a10ca6e8e8c939428936dbe5d25e3a0e2c19ebe0
pchchv <ipchchv@gmail.com>
2023-06-08 15:38:00 +0300

    Implement a divderiver type

--- division.go
+++ division.go
@@ -10,3 +10,12 @@ type divIntVals interface {
 	// getUpperDivisionValue gets the upper value for a division
 	getUpperDivisionValue() DivInt
 }
+
+type divderiver interface {
+	// deriveNew produces a new division with the same bit count as the old,
+	// but with the new values and prefix length
+	deriveNew(val, upperVal DivInt, prefLen PrefixLen) divisionValues
+	// derivePrefixed produces a new division with the same bit count and values as the old,
+	// but with the new prefix length
+	derivePrefixed(prefLen PrefixLen) divisionValues
+}
