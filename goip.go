04c1161a83f40799eb3efa78501e3f64d67378ae
pchchv <ipchchv@gmail.com>
2023-06-08 11:38:55 +0300

    Refactoring

--- goip.go
+++ goip.go
@@ -12,6 +12,16 @@ const (
 // IPVersion is the version type used by IP address types.
 type IPVersion string
 
+// IPAddress represents an IP address or subnet, either IPv4 or IPv6 (except zero IPAddress, which is neither).
+// An IP address consists of segments that have a range of values and may additionally have an associated prefix length.
+// An IPAddress with a null value has no segments, neither IPv4 nor IPv6,
+// which is not compatible with a null value for IPv4 or IPv6, which are 0.0.0.0 and :: respectively.
+// The null value is also known as adaptive zero.
+// To create it from a string, use NewIPAddressString and then use the ToAddress or GetAddress method from [IPAddressString].
+type IPAddress struct {
+	ipAddressInternal
+}
+
 // IsIPv4 returns true if this represents version 4.
 func (version IPVersion) IsIPv4() bool {
 	return len(version) == 4 && strings.EqualFold(string(version), string(IPv4))
