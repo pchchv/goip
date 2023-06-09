5cb3912d9f7b900b36faf25b1a3a280f17e46353
pchchv <ipchchv@gmail.com>
2023-06-09 13:09:33 +0300

    Implement a GetPrefixLen and isNil methods

--- large_division.go
+++ large_division.go
@@ -43,3 +43,22 @@ func (div *IPAddressLargeDivision) GetDivisionPrefixLen() PrefixLen {
 	return div.getDivisionPrefixLength()
 }
 
+// GetPrefixLen returns the network prefix for the unit.
+// For an address like "1.2.0.0/16", the network prefix is 16.
+// When it comes to each address subdivision or segment, the prefix for the subdivision is the prefix obtained when the address or partition prefix is applied.
+// For example, consider the address "1.2.0.0/20".
+// The first segment has no prefix because the address prefix 20 is beyond the 8 bits in the first segment, it is not even applied to the segment.
+// The second segment has no prefix because the address prefix extends beyond bits 9 through 16,
+// which are in the second segment, it also does not apply to this segment.
+// The third segment is prefixed with 4 because address prefix 20 corresponds to the first 4 bits in the third segment,
+// which means that the first 4 bits are part of the network portion of the address or segment.
+// The last segment is prefixed with 0 because no bits are part of the network portion of the address or segment.
+// The following division prefixes apply throughout the address: nil ... nil (1 to the bit length of the segment) 0 ... 0.
+// If the segment has no prefix, nil is returned.
+func (div *IPAddressLargeDivision) GetPrefixLen() PrefixLen {
+	return div.getDivisionPrefixLength()
+}
+
+func (div *IPAddressLargeDivision) isNil() bool {
+	return div == nil
+}
