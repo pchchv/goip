735326d7061a3d377bc40cebffd71ab91439fad8
pchchv <ipchchv@gmail.com>
2023-06-13 12:31:13 +0300

    Add an addressLargeDivInternal type

--- large_division.go
+++ large_division.go
@@ -4,6 +4,11 @@ import "math/big"
 
 type BigDivInt = big.Int
 
+type addressLargeDivInternal struct {
+	addressDivisionBase
+	defaultRadix *BigDivInt
+}
+
 // IPAddressLargeDivision represents an arbitrary bit size division in an address or address division grouping.
 // It can contain a single value or a range of consecutive values and has an assigned bit length.
 // Like all address components, it is immutable.
