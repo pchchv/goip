c82d573cacc8a36c37cd779e5f7122ac5494fa7f
pchchv <ipchchv@gmail.com>
2023-06-13 13:16:31 +0300

    Implement a nilString and nilSection funcs

---
+++ utils.go
@@ -0,0 +1,11 @@
+package goip
+
+func nilString() string {
+	return "<nil>"
+}
+
+// nilSection prints a string for sections with a nil division slice or division slice of 0 length.
+// For division groupings, the division slice string is generated from using the slice, see toString() or defaultFormat() in grouping code.
+func nilSection() string {
+	return ""
+}
