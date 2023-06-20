fab9ab69cc631f13e747f2e1d4b099b605e8f10f
pchchv <ipchchv@gmail.com>
2023-06-21 02:06:38 +0300

    Add some vars

---
+++ ipsection.go
@@ -0,0 +1,11 @@
+package goip
+
+import "github.com/pchchv/goip/address_string"
+
+var (
+	rangeWildcard                 = new(address_string.WildcardsBuilder).ToWildcards()
+	allWildcards                  = new(address_string.WildcardOptionsBuilder).SetWildcardOptions(address_string.WildcardsAll).ToOptions()
+	wildcardsRangeOnlyNetworkOnly = new(address_string.WildcardOptionsBuilder).SetWildcards(rangeWildcard).ToOptions()
+	allSQLWildcards               = new(address_string.WildcardOptionsBuilder).SetWildcardOptions(address_string.WildcardsAll).SetWildcards(
+		new(address_string.WildcardsBuilder).SetWildcard(SegmentSqlWildcardStr).SetSingleWildcard(SegmentSqlSingleWildcardStr).ToWildcards()).ToOptions()
+)
