15b330b832ae52c570e6b4cc5f8850e1565da7b8
pchchv <ipchchv@gmail.com>
2023-06-21 02:04:38 +0300

    Add some consts

---
+++ address.go
@@ -0,0 +1,18 @@
+package goip
+
+const (
+	HexPrefix                       = "0x"
+	OctalPrefix                     = "0"
+	BinaryPrefix                    = "0b"
+	RangeSeparator                  = '-'
+	RangeSeparatorStr               = "-"
+	AlternativeRangeSeparator       = '\u00bb'
+	AlternativeRangeSeparatorStr    = "\u00bb" // '»'
+	ExtendedDigitsRangeSeparatorStr = AlternativeRangeSeparatorStr
+	SegmentWildcard                 = '*'
+	SegmentWildcardStr              = "*"
+	SegmentSqlWildcard              = '%'
+	SegmentSqlWildcardStr           = "%"
+	SegmentSqlSingleWildcard        = '_'
+	SegmentSqlSingleWildcardStr     = "_"
+)
