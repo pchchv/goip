7311ee26c8f93c5dccb4a499b885e730eb71f923
pchchv <ipchchv@gmail.com>
2023-06-28 11:42:33 +0300

    Add some consts

--- ipv6_address.go
+++ ipv6_address.go
@@ -1,6 +1,36 @@
 package goip
 
-const NoZone = ""
+const (
+	NoZone                           = ""
+	IPv6SegmentSeparator             = ':'
+	IPv6SegmentSeparatorStr          = ":"
+	IPv6ZoneSeparator                = '%'
+	IPv6ZoneSeparatorStr             = "%"
+	IPv6AlternativeZoneSeparator     = '\u00a7'
+	IPv6AlternativeZoneSeparatorStr  = "\u00a7" //'§'
+	IPv6BitsPerSegment               = 16
+	IPv6BytesPerSegment              = 2
+	IPv6SegmentCount                 = 8
+	IPv6MixedReplacedSegmentCount    = 2
+	IPv6MixedOriginalSegmentCount    = 6
+	IPv6MixedOriginalByteCount       = 12
+	IPv6ByteCount                    = 16
+	IPv6BitCount                     = 128
+	IPv6DefaultTextualRadix          = 16
+	IPv6MaxValuePerSegment           = 0xffff
+	IPv6ReverseDnsSuffix             = ".ip6.arpa"
+	IPv6ReverseDnsSuffixDeprecated   = ".ip6.int"
+	IPv6UncSegmentSeparator          = '-'
+	IPv6UncSegmentSeparatorStr       = "-"
+	IPv6UncZoneSeparator             = 's'
+	IPv6UncZoneSeparatorStr          = "s"
+	IPv6UncRangeSeparator            = AlternativeRangeSeparator
+	IPv6UncRangeSeparatorStr         = AlternativeRangeSeparatorStr
+	IPv6UncSuffix                    = ".ipv6-literal.net"
+	IPv6SegmentMaxChars              = 4
+	ipv6BitsToSegmentBitshift        = 4
+	IPv6AlternativeRangeSeparatorStr = AlternativeRangeSeparatorStr
+)
 
 // Zone represents an IPv6 address zone or scope.
 type Zone string
