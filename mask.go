51216d76485d5e06d6cec4884bae6582231829b9
pchchv <ipchchv@gmail.com>
2023-06-29 11:56:37 +0300

    GetMaskedLower, GetMaskedUpper and IsSequential methods

--- mask.go
+++ mask.go
@@ -13,3 +13,18 @@ type Masker interface {
 type maskerBase struct {
 	isSequentialVal bool
 }
+
+// GetMaskedLower provides the smallest masked value, which is not necessarily the smallest masked value.
+func (masker maskerBase) GetMaskedLower(value, maskValue uint64) uint64 {
+	return value & maskValue
+}
+
+// GetMaskedUpper provides the largest masked value, which is not necessarily the largest masked value.
+func (masker maskerBase) GetMaskedUpper(upperValue, maskValue uint64) uint64 {
+	return upperValue & maskValue
+}
+
+// IsSequential returns whether masking all values in a range results in a consistent set of values.
+func (masker maskerBase) IsSequential() bool {
+	return masker.isSequentialVal
+}
