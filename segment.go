a51715ba7706f23125dbd796dc2b2d97a6f41ae4
pchchv <ipchchv@gmail.com>
2023-07-03 11:29:52 +0300

    Implement isReversibleRange method

--- segment.go
+++ segment.go
@@ -190,6 +190,130 @@ func (seg *addressSegmentInternal) GetSegmentHostMask(networkBits BitCount) SegI
 	return ^(^SegInt(0) << uint(bitCount-networkBits))
 }
 
+func (seg *addressSegmentInternal) isReversibleRange(perByte bool) (isReversible bool) {
+	// Consider the case of range bit reversing.
+	// Any range that can be reversed successfully must span all bits
+	// (otherwise after flipping you will get a range where the lowest bit is constant, which is impossible in any contiguous range).
+	// This means that at least one value is 0xxxx and the other is 1xxxx (using 5 bits for our example).
+	// This means that you should have values 01111 and 10000, since the range is continuous.
+	// But if you reversing a range twice, you get the original again,
+	// so the reversed range must also be reversible, so the reversed range also has 01111 and 10000.
+	// This means that in both the original and reversed ranges the two patterns are also flipped, namely 00001 and 11110.
+	// This means that both ranges must span from at most 1 to at least 11110.
+	// However, the two remaining values, 0 and 11111, are optional because they are boundary values and remain themselves when flipped,
+	// and thus have no effect on whether the reversible range is continuous.
+	// Thus, the only reversible ranges are 0-11111, 0-11110, 1-11110, and 1-11111.
+	// -----------------------
+	// Consider the case of reversing each of the bytes of a range.
+	//
+	// You can apply the same argument to the top multiple byte, which means it is 0 or 1 to 254 or 255.
+	// Suppose it is followed by another byte.
+	// If you take the range of the upper byte and hold it constant, then reversing the next byte applies the same argument to that byte.
+	// Thus, the lower byte should span from at most 1 to at least 11111110.
+	// This argument also applies when the value of the upper byte is constant.
+	// Thus, at any value, the lower byte must span from at most 1 to no at least 111111110.
+	// So you have x 00000001-x 111111110 and y 00000001-y 111111110 and so on.
+	//
+	// But all bytes form a range, so you must also have values between them.
+	//
+	// This means that you have 1 00000001 - 1 111111110 - 10 111111110 - 11 111111110 up to x 11111110, where x is at least 11111110.
+	// In all cases, the upper byte lower value is at most 1, and 1 < 10000000.
+	// This means that you always have 10000000 00000000.
+	// So you also have an reverse value (as argued above, for any value we have an reverse value).
+	// So you always have 00000001 00000000.
+	//
+	// In other words, if the upper byte has a lower 0, then the full low byte must be at most 0 00000001.
+	// Otherwise, if the upper byte has a lower 1, then the full bytes lower is at most 1 00000000.
+	//
+	// In other words, if any upper byte has a lower value 1, then all lower values to follow are 0.
+	// If all upper bytes have a lower value 0, then the next byte is permitted to have a lower value 1.
+	//
+	// In general, any upper byte that has a lower of 1 forces the remaining lower values to be 0.
+	//
+	// If all upper bytes are zero, and thus the lower is at most 0 0 0 0 1, then the only remaining lower value is 0 0 0 0 0.
+	// This value reverses to itself, so it is optional.
+	//
+	// The same argument applies to upper bounds.
+	// ----------------------
+	// Consider the case of reversing the bytes of a range.
+	// Any range that can be reversed successfully must span all bits
+	// (otherwise after flipping you will have a range where the lowest bit is constant, which is impossible in any contiguous range).
+	// This means that at least one value is 0xxxxx and the other is 1xxxxx (we use 6 bits for our example and assume that each byte has 3 bits).
+	// This means that you should have the values 011111 and 100000 because the range is continuous.
+	// But if you reversing the range twice, you get the original again, so the reversed range must also be reversible,
+	// so the reversed range also has 011111 and 100000.
+	//
+	// This means that both the original and the reversed also have these two bytes in each flipped, namely 111011 and 000100.
+	// Thus, the range must have 000100, 011111, 100000, 111011, so it must be at least 000100 to 111011.
+	// But what if the range does not have 000001?
+	// Then the reversed range cannot have 001000, the byte-reversed address.
+	// But we know that it spans from 000100 to 111011.
+	// So the original must have 000001. What if it doesn't have 111110?
+	// Then the reverse can't have 110111, a byte-reversed address.
+	// But we know it's between 000100 and 111011.
+	// So the original must have 111110.
+	// So it must be in the range 000001 to 111110.
+	// That leaves only the values 000000 and 111111.
+	// But again, the two remaining values are optional, because the byte-reverse to themselves.
+	// Thus, in the case of byte-reverse, we have the same potential ranges as in the case of bit-reverse: 0-111111, 0-111110, 1-111110, and 1-111111.
+	if perByte {
+		byteCount := seg.GetByteCount()
+		bitCount := seg.GetBitCount()
+		val := seg.GetSegmentValue()
+		upperVal := seg.GetUpperSegmentValue()
+		for i := 1; i <= byteCount; i++ {
+			bitShift := i << 3
+			shift := bitCount - BitCount(bitShift)
+			byteVal := val >> uint(shift)
+			upperByteVal := upperVal >> uint(shift)
+			if byteVal != upperByteVal {
+				if byteVal > 1 || upperByteVal < 254 {
+					return false
+				}
+				i++
+				if i <= byteCount {
+					lowerIsZero := byteVal == 1
+					upperIsMax := upperByteVal == 254
+					for {
+						bitShift = i << 3
+						shift = bitCount - BitCount(bitShift)
+						byteVal = val >> uint(shift)
+						upperByteVal = upperVal >> uint(shift)
+						if lowerIsZero {
+							if byteVal != 0 {
+								return
+							}
+						} else {
+							if byteVal > 1 {
+								return
+							}
+							lowerIsZero = byteVal == 1
+						}
+						if upperIsMax {
+							if upperByteVal != 255 {
+								return
+							}
+						} else {
+							if upperByteVal < 254 {
+								return
+							}
+							upperIsMax = upperByteVal == 254
+						}
+						i++
+						if i > byteCount {
+							break
+						}
+					}
+				}
+				return true
+			}
+		}
+		return true
+	}
+	isReversible = seg.GetSegmentValue() <= 1 && seg.GetUpperSegmentValue() >= seg.GetMaxValue()-1
+	return
+}
+
 func segValsSame(oneVal, twoVal, oneUpperVal, twoUpperVal SegInt) bool {
 	return oneVal == twoVal && oneUpperVal == twoUpperVal
 }
