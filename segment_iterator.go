dfa7e4af2573b10d988a31d8bc3f259dbbfab9ae
pchchv <ipchchv@gmail.com>
2023-07-05 08:30:38 +0300

    Implement a HasNext and Next methods

--- segment_iterator.go
+++ segment_iterator.go
@@ -3,3 +3,15 @@ package goip
 type singleSegmentIterator struct {
 	original *AddressSegment
 }
+
+func (it *singleSegmentIterator) HasNext() bool {
+	return it.original != nil
+}
+
+func (it *singleSegmentIterator) Next() (res *AddressSegment) {
+	if it.HasNext() {
+		res = it.original.toAddressSegment()
+		it.original = nil
+	}
+	return
+}
