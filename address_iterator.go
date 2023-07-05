886ee4ebe5e155215d722c5a0d8133dfbff5bfd9
pchchv <ipchchv@gmail.com>
2023-07-05 08:25:51 +0300

    Add an Iterator type

---
+++ address_iterator.go
@@ -0,0 +1,7 @@
+package goip
+
+// Iterator iterates collections such as subnets and consecutive address ranges.
+type Iterator[T any] interface {
+	HasNext() bool // returns true if there is another element to iterate, false otherwise
+	Next() T       // returns the next item, or the zero value for T if there is none left
+}
