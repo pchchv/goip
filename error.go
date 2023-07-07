1e6cac327347e7c5ba377f91c24d1ec62ca3de2b
pchchv <ipchchv@gmail.com>
2023-07-07 08:08:14 +0300

    Add an addressError type

--- error.go
+++ error.go
@@ -1,5 +1,10 @@
 package goip
 
+type addressError struct {
+	key string // to look up the error message
+	str string // an optional string with the address
+}
+
 func getStr(str string) (res string) {
 	if len(str) > 0 {
 		res = str + " "
