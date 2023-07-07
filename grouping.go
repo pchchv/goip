771c141e3c7b8a00acf7b5993edc847cbeaf5059
pchchv <ipchchv@gmail.com>
2023-07-07 21:54:17 +0300

    Add an AddressDivisionGrouping type

--- grouping.go
+++ grouping.go
@@ -5,3 +5,20 @@ var emptyBytes = make([]byte, 0, 0)
 type addressDivisionGroupingInternal struct {
 	addressDivisionGroupingBase
 }
+
+// AddressDivisionGrouping objects consist of a series of AddressDivision objects,
+// each containing a consistent range of values.
+//
+// AddressDivisionGrouping objects are immutable.
+// This also makes them concurrency-safe.
+//
+// AddressDivision objects use uint64 to represent their values,
+// so this places a limit on the size of the divisions in AddressDivisionGrouping.
+//
+// AddressDivisionGrouping objects are similar to address sections and addresses,
+// except that groupings can have divisions of different bit-lengths,
+// including divisions that are not the exact number of bytes,
+// whereas all segments in an address or address section must have the same bit size and exact number of bytes.
+type AddressDivisionGrouping struct {
+	addressDivisionGroupingInternal
+}
