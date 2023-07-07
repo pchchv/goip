b1b773e6389f882f4605edf6e5d908865ee364f2
pchchv <ipchchv@gmail.com>
2023-07-07 08:08:37 +0300

    Implement an Error method

--- error.go
+++ error.go
@@ -5,6 +5,10 @@ type addressError struct {
 	str string // an optional string with the address
 }
 
+func (a *addressError) Error() string {
+	return getStr(a.str) + lookupStr("ipaddress.address.error") + " " + lookupStr(a.key)
+}
+
 func getStr(str string) (res string) {
 	if len(str) > 0 {
 		res = str + " "
