24f7a766581a2f5acc565cf65aa4edfff2deda97
pchchv <ipchchv@gmail.com>
2023-07-10 08:12:08 +0300

    Add an ExtendedIdentifierString

---
+++ framework__string_wrappers.go
@@ -0,0 +1,10 @@
+package goip
+
+// ExtendedIdentifierString is a common interface for strings that identify hosts,
+// namely [IPAddressString], [MACAddressString] and [HostName].
+type ExtendedIdentifierString interface {
+	HostIdentifierString
+	GetAddress() AddressType         // returns the identified address or nil if none
+	ToAddress() (AddressType, error) // returns the identified address or an error.
+	Unwrap() HostIdentifierString    // returns the wrapped IPAddressString, MACAddressString or HostName as an interface, HostIdentifierString.
+}
