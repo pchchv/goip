6d5c12cec373f5f3c4323ec5bbf38370f96f0916
pchchv <ipchchv@gmail.com>
2023-07-10 08:34:46 +0300

    Add an Address type

--- address.go
+++ address.go
@@ -51,3 +51,30 @@ type addressInternal struct {
 	zone    Zone
 	cache   *addressCache
 }
+
+// Address represents a single address or a set of multiple addresses, such as an IP subnet or a set of MAC addresses.
+//
+// Addresses consist of a sequence of segments, each with the same bit-size.
+// The number of such segments and the bit-size are determined by the underlying version or type of address, whether IPv4, IPv6, MAC, or other.
+// Each segment can represent a single value or a sequential range of values.
+// Addresses can also have an appropriate prefix length - the number of consecutive bits that make up the prefix, the most significant bits of the address.
+//
+// To create an address from a string, use NewIPAddressString or NewMACAddressString,
+// then use the ToAddress or GetAddress methods to get [IPAddress] or [MACAddress] and then you can convert it to that type using the ToAddressBase method.
+//
+// Any specific address types can be converted to Address using the ToAddressBase method
+// and then returned to the original types using methods such as ToIPv6, ToIP, ToIPv4 and ToMAC.
+// When such a method is called for a given address,
+// if the address was not originally constructed as the type returned by the method, the method will return nil.
+// Conversion methods work with nil pointers (return nil), so they can be safely chained together.
+//
+// This allows you to create polymorphic code that works with all addresses, like the address triplet code in this library,
+// while at the same time allowing methods and code specific to each version or address type.
+//
+// You can also use the IsIPv6, IsIP, IsIPv4 and IsMAC methods,
+// which will return true if and only if the corresponding ToIPv6, ToIP, ToIPv4 and ToMAC methods return non-nil, respectively.
+//
+// A zero value for an address is an address with no segments and no associated version or type of address, also known as adaptive zero.
+type Address struct {
+	addressInternal
+}
