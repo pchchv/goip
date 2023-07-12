857efbef7531f9e5c33f71d63fbd394e309b9250
pchchv <ipchchv@gmail.com>
2023-07-12 09:23:37 +0300

    Add an IPv6v4MixedAddressGrouping type

---
+++ ipv6_section.go
@@ -0,0 +1,7 @@
+package goip
+
+// IPv6v4MixedAddressGrouping has divisions that are a mix of IPv6 and IPv4 sections.
+// It has an initial IPv6 section followed by an IPv4 section.
+type IPv6v4MixedAddressGrouping struct {
+	addressDivisionGroupingInternal
+}
