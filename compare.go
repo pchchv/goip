bdf202d1412595f3a64bbeebd58e2c8badd29a3f
pchchv <ipchchv@gmail.com>
2023-07-13 08:27:50 +0300

    Add an AddressComparator type

--- compare.go
+++ compare.go
@@ -9,3 +9,10 @@ type componentComparator interface {
 	compareValues(oneUpper, oneLower, twoUpper, twoLower uint64) int
 	compareLargeValues(oneUpper, oneLower, twoUpper, twoLower *big.Int) int
 }
+
+// AddressComparator has methods for comparing addresses, or sections, or series of divisions, or segments, or divisions, or consecutive ranges.
+// The AddressComparator also allows any two instances of any such address items to be compared using the Compare method.
+// A zero value acts as CountComparator, the default comparator.
+type AddressComparator struct {
+	componentComparator componentComparator
+}
