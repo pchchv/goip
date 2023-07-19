75bd18b576dd6d832ffaf04dd554bb22e024cc61
pchchv <ipchchv@gmail.com>
2023-07-19 10:36:35 +0300

    Add some methods of ipAddressStringFormatParameters

--- address_string_param/ip_address_string_params.go
+++ address_string_param/ip_address_string_params.go
@@ -6,3 +6,18 @@ type ipAddressStringFormatParameters struct {
 	noPrefixLengthLeadingZeros,
 	noBinary bool
 }
+
+// AllowsPrefixesBeyondAddressSize allows prefix length values greater than 32 for IPv4 or greater than 128 for IPv6.
+func (params *ipAddressStringFormatParameters) AllowsPrefixesBeyondAddressSize() bool {
+	return params.allowPrefixesBeyondAddrSize
+}
+
+// AllowsPrefixLenLeadingZeros allows leading zeros in the prefix length like "1.2.3.4/016".
+func (params *ipAddressStringFormatParameters) AllowsPrefixLenLeadingZeros() bool {
+	return !params.noPrefixLengthLeadingZeros
+}
+
+// AllowsBinary allows binary addresses like "11111111.0.1.0" or "1111111111111111::".
+func (params *ipAddressStringFormatParameters) AllowsBinary() bool {
+	return !params.noBinary
+}
