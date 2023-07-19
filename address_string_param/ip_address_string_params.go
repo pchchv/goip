2a8bca9489244c8521576133e50a6382c600b90b
pchchv <ipchchv@gmail.com>
2023-07-19 10:35:44 +0300

    Add an ipAddressStringFormatParameters type

---
+++ address_string_param/ip_address_string_params.go
@@ -0,0 +1,8 @@
+package address_string_param
+
+type ipAddressStringFormatParameters struct {
+	addressStringFormatParameters
+	allowPrefixesBeyondAddrSize,
+	noPrefixLengthLeadingZeros,
+	noBinary bool
+}
