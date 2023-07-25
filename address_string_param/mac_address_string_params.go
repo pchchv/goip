1595407982bc6ece4be5830860ed587fe8fefd6d
pchchv <ipchchv@gmail.com>
2023-07-25 21:32:25 +0300

    Add a MACAddressStringParams type

--- address_string_param/mac_address_string_params.go
+++ address_string_param/mac_address_string_params.go
@@ -16,3 +16,26 @@ type MACAddressStringFormatParams interface {
 	// AllowsShortSegments allows segments that are just a single hex digit and not two.
 	AllowsShortSegments() bool
 }
+
+// MACAddressStringParams provides parameters for parsing MAC address strings.
+// This allows you to control the validation performed by MACAddressString.
+// MACAddressString uses the default MACAddressStringParams resolving instance if one is not specified.
+// If you want to use parameters other than the default, use this interface.
+// Immutable instances can be constructed using the MACAddressStringParamsBuilder.
+type MACAddressStringParams interface {
+	AddressStringParams
+	// GetPreferredLen indicates whether an ambiguous address like * is considered to be MAC 6 bytes, EUI-64 8 bytes, or either one.
+	GetPreferredLen() MACAddressLen
+	// AllowsDashed allows addresses like "aa-bb-cc-dd-ee-ff".
+	AllowsDashed() bool
+	// AllowsSingleDashed allows addresses like "aabbcc-ddeeff".
+	AllowsSingleDashed() bool
+	// AllowsColonDelimited allows addresses like "aa:bb:cc:dd:ee:ff".
+	AllowsColonDelimited() bool
+	// AllowsDotted allows addresses like "aaa.bbb.ccc.ddd".
+	AllowsDotted() bool
+	// AllowsSpaceDelimited allows addresses like "aa bb cc dd ee ff".
+	AllowsSpaceDelimited() bool
+	// GetFormatParams returns the parameters that apply to formatting of the address segments.
+	GetFormatParams() MACAddressStringFormatParams
+}
