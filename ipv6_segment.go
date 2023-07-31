ad18d94c914031c70b2fe58db40a974e3298a36a
pchchv <ipchchv@gmail.com>
2023-07-31 08:50:17 +0300

    Add an ipv6SegmentValues type

--- ipv6_segment.go
+++ ipv6_segment.go
@@ -1,5 +1,14 @@
 package goip
 
+const useIPv6SegmentCache = true
+
 type IPv6SegInt = uint16
 
 type IPv6SegmentValueProvider func(segmentIndex int) IPv6SegInt
+
+type ipv6SegmentValues struct {
+	value      IPv6SegInt
+	upperValue IPv6SegInt
+	prefLen    PrefixLen
+	cache      divCache
+}
