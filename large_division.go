f735fbacae5536b21feb046129921e2308c7f318
pchchv <ipchchv@gmail.com>
2023-08-04 09:08:00 +0300

    Implement a String method

--- large_division.go
+++ large_division.go
@@ -655,6 +655,20 @@ func (div *IPAddressLargeDivision) adjustUpperLeadingZeroCount(leadingZeroCount 
 	return div.adjustLeadingZeroCount(leadingZeroCount, div.getUpperValue(), radix)
 }
 
+// String produces a string that is useful when a division string is provided with no context.
+// It uses a string prefix for octal or hex ("0" or "0x"), and does not use the wildcard '*',
+// because division size is variable, so '*' is ambiguous.
+// GetWildcardString is more appropriate in context with other segments or divisions.
+// It does not use a string prefix and uses '*' for full-range segments.
+// GetString is more appropriate in context with prefix lengths,
+// it uses zeros instead of wildcards for prefix block ranges.
+func (div *IPAddressLargeDivision) String() string {
+	if div == nil {
+		return nilString()
+	}
+	return div.toString()
+}
+
 type largeDivValues struct {
 	bitCount         BitCount
 	value            *BigDivInt
