0e4a78c94f91149660d84f633171d4a4d0cdf8ff
pchchv <ipchchv@gmail.com>
2023-08-07 13:23:02 +0300

    Implement a reverseUint32 func

--- utils.go
+++ utils.go
@@ -207,3 +207,13 @@ func reverseUint16(b uint16) uint16 {
 	x = ((x & 0xf0f0) >> 4) | ((x & 0x0f0f) << 4)
 	return (x >> 8) | (x << 8)
 }
+
+func reverseUint32(i uint32) uint32 {
+	x := i
+	x = ((x & 0xaaaaaaaa) >> 1) | ((x & 0x55555555) << 1)
+	x = ((x & 0xcccccccc) >> 2) | ((x & 0x33333333) << 2)
+	x = ((x & 0xf0f0f0f0) >> 4) | ((x & 0x0f0f0f0f) << 4)
+	x = ((x & 0xff00ff00) >> 8) | ((x & 0x00ff00ff) << 8)
+	return (x >> 16) | (x << 16)
+}
+
