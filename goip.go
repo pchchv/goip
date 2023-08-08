66ec1f2154df686d91c9f79b2e969830f41607bf
pchchv <ipchchv@gmail.com>
2023-08-08 12:45:06 +0300

    Implement a GetBlockCount and GetPrefixLen methods

--- goip.go
+++ goip.go
@@ -1,6 +1,7 @@
 package goip
 
 import (
+	"math/big"
 	"strings"
 	"unsafe"
 )
@@ -99,4 +100,29 @@ func (addr *ipAddressInternal) GetPrefixCount() *big.Int {
 // If not a subnet of multiple addresses, or a subnet with just single prefix of the given length, returns 1.
 func (addr *ipAddressInternal) GetPrefixCountLen(prefixLen BitCount) *big.Int {
 	return addr.addressInternal.GetPrefixCountLen(prefixLen)
+}
+
+// GetBlockCount returns the count of distinct values in the given number of initial (more significant) segments.
+func (addr *ipAddressInternal) GetBlockCount(segments int) *big.Int {
+	return addr.addressInternal.GetBlockCount(segments)
+}
+
+// GetPrefixLen returns the prefix length, or nil if there is no prefix length.
+//
+// A prefix length indicates the number of bits in the initial part of the address that comprise the prefix.
+//
+// A prefix is a part of the address that is not specific to that address but common amongst a group of addresses,
+// such as a CIDR prefix block subnet.
+//
+// For IP addresses, the prefix is explicitly defined when the address is created.
+// For example, "1.2.0.0/16" has a prefix length of 16, while "1.2.*.*" has no prefix length,
+// even though they both represent the same set of addresses and are considered equal.
+// Prefixes can be considered variable for a given IP address and can depend on routing.
+//
+// The methods GetMinPrefixLenForBlock and GetPrefixLenForSingleBlock can help you
+// to obtain or define a prefix length if one does not exist already.
+// The method ToPrefixBlockLen allows you to create the subnet consisting of
+// the block of addresses for any given prefix length.
+func (addr *ipAddressInternal) GetPrefixLen() PrefixLen {
+	return addr.addressInternal.GetPrefixLen()
 }
