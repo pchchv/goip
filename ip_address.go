41ae78f1d3d9e34b39f0bdf216c6ca1eb321d31b
pchchv <ipchchv@gmail.com>
2023-08-08 12:21:41 +0300

    Implement a toIPAddress method

--- ip_address.go
+++ ip_address.go
@@ -1,6 +1,12 @@
 package goip
 
+import "unsafe"
+
 // necessary to avoid direct access to IPAddress
 type ipAddressInternal struct {
 	addressInternal
+}
+
+func (addr *ipAddressInternal) toIPAddress() *IPAddress {
+	return (*IPAddress)(unsafe.Pointer(addr))
 }
