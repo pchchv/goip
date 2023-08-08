5f7dd3e4b618419e103ad568d6563f8f37661223
pchchv <ipchchv@gmail.com>
2023-08-08 12:11:13 +0300

    Add a SequentialRange type

--- ip_sequential_range.go
+++ ip_sequential_range.go
@@ -45,3 +45,30 @@ type SequentialRangeConstraint[T any] interface {
 	getLowestHighestAddrs() (lower, upper T)
 	getAddrType() addrType
 }
+
+// SequentialRange represents an arbitrary range of consecutive IP addresses, starting from the lowest address and ending at the top address, inclusive.
+//
+// For a generic T type, you can select *IPAddress, *IPv4Address, or *IPv6Address.
+//
+// This type allows any sequential range of addresses to be represented, including those that cannot be represented in [IPAddress] or [IPAddressString].
+//
+// [IPAddress] and [IPAddressString] allow a range of values to be specified for each segment,
+// allowing single addresses, any address subnet with a CIDR prefix
+// (e.g., "1.2.0.0/16" or "1:2:3:4::/64"), or any subnet that can be represented using segment ranges (e.g., "1.2.0-255.*" or "1:2:3:4:*").
+// See [IPAddressString] for more details.
+// [IPAddressString] and [IPAddress] cover all potential subnets and addresses that can be represented by
+// a single address string of 4 or less segments for IPv4 and 8 or less segments for IPv6.
+// In contrast, this type covers any sequential address range.
+//
+// String representations of this type include the full address for both the lower and upper bounds of the range.
+//
+// A zero value represents the range from the zero-valued of [IPAddress] to itself.
+//
+// For a range of type SequentialRange[*IPAddress], the range extends from an IPv4 address to another IPv4 address or from an IPv6 address to another IPv6 address.
+// A sequential range cannot include both IPv4 and IPv6 addresses.
+type SequentialRange[T SequentialRangeConstraint[T]] struct {
+	lower      T
+	upper      T
+	isMultiple bool // set on construction, even for zero values
+	cache      *rangeCache
+}
