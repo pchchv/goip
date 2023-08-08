cd7a2dab6d9e242fa7309c267a665a9775afb2ef
pchchv <ipchchv@gmail.com>
2023-08-08 12:44:04 +0300

    Implement a GetPrefixCount and GetPrefixCountLen methods

--- goip.go
+++ goip.go
@@ -82,3 +82,21 @@ type ipAddressInternal struct {
 func (addr *ipAddressInternal) toIPAddress() *IPAddress {
 	return (*IPAddress)(unsafe.Pointer(addr))
 }
+
+// GetPrefixCount returns the count of prefixes in this address or subnet.
+//
+// The prefix length is given by GetPrefixLen.
+//
+// If this has a non-nil prefix length, returns the count of the range of values in the prefix.
+//
+// If this has a nil prefix length, returns the same value as GetCount.
+func (addr *ipAddressInternal) GetPrefixCount() *big.Int {
+	return addr.addressInternal.GetPrefixCount()
+}
+
+// GetPrefixCountLen returns the count of prefixes in this address or subnet for the given prefix length.
+//
+// If not a subnet of multiple addresses, or a subnet with just single prefix of the given length, returns 1.
+func (addr *ipAddressInternal) GetPrefixCountLen(prefixLen BitCount) *big.Int {
+	return addr.addressInternal.GetPrefixCountLen(prefixLen)
+}
