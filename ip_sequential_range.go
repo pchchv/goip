698d862d3bec3b76b13961a9fc9e0c0c6ded23ec
pchchv <ipchchv@gmail.com>
2023-08-08 12:05:00 +0300

    Add a SequentialRangeConstraint type

--- ip_sequential_range.go
+++ ip_sequential_range.go
@@ -1,7 +1,47 @@
 package goip
 
-import "math/big"
+import (
+	"fmt"
+	"math/big"
+)
+
+// DefaultSeqRangeSeparator is the low to high value separator used when creating strings for IP ranges.
+const DefaultSeqRangeSeparator = " -> "
 
 type rangeCache struct {
 	cachedCount *big.Int
+}
+
+// SequentialRangeConstraint is the generic type constraint for an IP address sequential range.
+type SequentialRangeConstraint[T any] interface {
+	// cannot use IPAddressType here because ToAddressString() results in a circular dependency,
+	// SequentialRangeConstraint -> IPAddressType -> IPAddressString -> SequentialRange -> SequentialRangeConstraint
+	AddressType
+	IPAddressRange
+	comparable
+	ToIP() *IPAddress
+	PrefixedConstraint[T]
+	Increment(int64) T
+	GetLower() T
+	GetUpper() T
+	CoverWithPrefixBlockTo(T) T
+	SpanWithPrefixBlocksTo(T) []T
+	SpanWithSequentialBlocksTo(T) []T
+	SpanWithPrefixBlocks() []T
+	IncludesZeroHostLen(BitCount) bool
+	IncludesMaxHostLen(BitCount) bool
+	Format(state fmt.State, verb rune)
+	rangeIterator(upper T,
+		valsAreMultiple bool,
+		prefixLen PrefixLen,
+		segProducer func(addr *IPAddress, index int) *IPAddressSegment,
+		segmentIteratorProducer func(seg *IPAddressSegment, index int) Iterator[*IPAddressSegment],
+		segValueComparator func(seg1, seg2 *IPAddress, index int) bool,
+		networkSegmentIndex,
+		hostSegmentIndex int,
+		prefixedSegIteratorProducer func(seg *IPAddressSegment, index int) Iterator[*IPAddressSegment],
+	) Iterator[T]
+	equalsSameVersion(AddressType) bool
+	getLowestHighestAddrs() (lower, upper T)
+	getAddrType() addrType
 }
