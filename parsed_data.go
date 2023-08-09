806aa424c0ca2abe0f5631e096281aea8b1e04eb
pchchv <ipchchv@gmail.com>
2023-08-09 14:12:41 +0300

    Add an addressParseData type

---
+++ parsed_data.go
@@ -0,0 +1,14 @@
+package goip
+
+type addressParseData struct {
+	segmentData                []uint32
+	segmentCount               int
+	anyWildcard                bool
+	isEmpty                    bool
+	isAllVal                   bool
+	isSingleSegmentVal         bool
+	consecutiveSepIndex        int
+	consecutiveSepSegmentIndex int
+	addressEndIndex            int
+	str                        string
+}
