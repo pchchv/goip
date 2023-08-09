cad3beb4f816f4a3d1806761e3f504c78c8e8d31
pchchv <ipchchv@gmail.com>
2023-08-09 14:01:18 +0300

    Add a embeddedAddress type

--- parsed_host.go
+++ parsed_host.go
@@ -1,6 +1,15 @@
 package goip
 
+import "github.com/pchchv/goip/address_error"
+
 type parsedHostCache struct {
 	normalizedLabels []string
 	host             string
+}
+
+type embeddedAddress struct {
+	isUNCIPv6Literal   bool
+	isReverseDNS       bool
+	addressStringError address_error.AddressStringError
+	addressProvider    ipAddressProvider
 }
