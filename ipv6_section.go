aef4005a8752b2370e6ee3b8faf776847b7d80a9
pchchv <ipchchv@gmail.com>
2023-08-21 07:12:42 +0300

    Add an embeddedIPv6AddressSection type

--- ipv6_section.go
+++ ipv6_section.go
@@ -6,6 +6,10 @@ type IPv6AddressSection struct {
 	ipAddressSectionInternal
 }
 
+type embeddedIPv6AddressSection struct {
+	IPv6AddressSection
+}
+
 // IPv6v4MixedAddressGrouping has divisions that are a mix of IPv6 and IPv4 sections.
 // It has an initial IPv6 section followed by an IPv4 section.
 type IPv6v4MixedAddressGrouping struct {
