f14f739ca6d85411287a908834556d429177d8fe
pchchv <ipchchv@gmail.com>
2023-08-21 07:10:58 +0300

    Add an IPv4AddressSection type

---
+++ ipv4_section.go
@@ -0,0 +1,7 @@
+package goip
+
+// IPv4AddressSection represents a section of an IPv4 address comprising 0 to 4 IPv4 address segments.
+// The zero values is a section with zero-segments.
+type IPv4AddressSection struct {
+	ipAddressSectionInternal
+}
