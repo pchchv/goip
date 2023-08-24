6bc5dcfcb5648b91b6060bd62b3837e7a59240b1
pchchv <ipchchv@gmail.com>
2023-08-24 13:21:02 +0300

    Add some consts

---
+++ mac_address.go
@@ -0,0 +1,23 @@
+package goip
+
+const (
+	MACBitsPerSegment                             = 8
+	MACBytesPerSegment                            = 1
+	MACDefaultTextualRadix                        = 16
+	MACMaxValuePerSegment                         = 0xff
+	MACMaxValuePerDottedSegment                   = 0xffff
+	MediaAccessControlSegmentCount                = 6
+	MediaAccessControlDottedSegmentCount          = 3
+	MediaAccessControlDotted64SegmentCount        = 4
+	ExtendedUniqueIdentifier48SegmentCount        = MediaAccessControlSegmentCount
+	ExtendedUniqueIdentifier64SegmentCount        = 8
+	MACOrganizationalUniqueIdentifierSegmentCount = 3
+	MACSegmentMaxChars                            = 2
+	MACDashSegmentSeparator                       = '-'
+	MACColonSegmentSeparator                      = ':'
+	MacSpaceSegmentSeparator                      = ' '
+	MacDottedSegmentSeparator                     = '.'
+	MacDashedSegmentRangeSeparator                = '|'
+	MacDashedSegmentRangeSeparatorStr             = "|"
+	macBitsToSegmentBitshift                      = 3
+)
