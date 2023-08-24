5de13634b3f63f2393d7594b897e8a03983608d4
pchchv <ipchchv@gmail.com>
2023-08-24 13:26:32 +0300

    Add a MACAddress type

--- mac_address.go
+++ mac_address.go
@@ -22,6 +22,18 @@ const (
 	macBitsToSegmentBitshift                      = 3
 )
 
+// MACAddress represents a MAC address or a collection of multiple individual MAC addresses.
+// Each segment may represent a single byte value or a range of byte values.
+//
+// A MAC address can be constructed from a byte slice, from a uint64, from a SegmentValueProvider,
+// from a MACAddressSection of 6 or 8 segments, or from an array of 6 or 8 MACAddressSegment instances.
+//
+// To create a string from a string,
+// use NewMACAddressString and then use the ToAddress or GetAddress method from [MACAddressString].
+type MACAddress struct {
+	addressInternal
+}
+
 func getMacSegCount(isExtended bool) (segmentCount int) {
 	if isExtended {
 		segmentCount = ExtendedUniqueIdentifier64SegmentCount
