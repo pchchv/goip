1c4f31a7adaa24319e33de2e864a016df63fabbf
pchchv <ipchchv@gmail.com>
2023-08-24 13:06:15 +0300

    Implement a ToSectionBase and ToDivGrouping methods

--- mac_section.go
+++ mac_section.go
@@ -16,3 +16,23 @@ func (section *MACAddressSection) IsMultiple() bool {
 func (section *MACAddressSection) IsPrefixed() bool {
 	return section != nil && section.isPrefixed()
 }
+
+// ToSectionBase converts to an AddressSection,
+// a polymorphic type usable with all address sections.
+// Afterwards, you can convert back with ToMAC.
+//
+// ToSectionBase can be called with a nil receiver,
+// enabling you to chain this method with methods that might return a nil pointer.
+func (section *MACAddressSection) ToSectionBase() *AddressSection {
+	return (*AddressSection)(section)
+}
+
+// ToDivGrouping converts to an AddressDivisionGrouping,
+// a polymorphic type usable with all address sections and division groupings.
+// Afterwards, you can convert back with ToMAC.
+//
+// ToDivGrouping can be called with a nil receiver,
+// enabling you to chain this method with methods that might return a nil pointer.
+func (section *MACAddressSection) ToDivGrouping() *AddressDivisionGrouping {
+	return section.ToSectionBase().ToDivGrouping()
+}
