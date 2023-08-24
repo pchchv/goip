bef0773aaee9bf56b90a28dc1a24f533a57646e5
pchchv <ipchchv@gmail.com>
2023-08-24 12:43:59 +0300

    Add a MACAddressSection type

---
+++ mac_section.go
@@ -0,0 +1,8 @@
+package goip
+
+// MACAddressSection is a section of a MACAddress.
+//
+// It is a series of 0 to 8 individual MAC address segments.
+type MACAddressSection struct {
+	addressSectionInternal
+}
