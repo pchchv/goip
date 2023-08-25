f975e1ce169075e2e663731e475aad384010ef8b
pchchv <ipchchv@gmail.com>
2023-08-25 22:19:27 +0300

    Implement a newMACSegmentValues func

--- mac_segment.go
+++ mac_segment.go
@@ -139,3 +139,17 @@ func newMACSegmentVal(value MACSegInt) *macSegmentValues {
 	}
 	return &macSegmentValues{value: value, upperValue: value}
 }
+
+func newMACSegmentValues(value, upperValue MACSegInt) *macSegmentValues {
+	if value == upperValue {
+		return newMACSegmentVal(value)
+	} else if value > upperValue {
+		value, upperValue = upperValue, value
+	}
+
+	if useMACSegmentCache && value == 0 && upperValue == MACMaxValuePerSegment {
+		return allRangeValsMAC
+	}
+
+	return &macSegmentValues{value: value, upperValue: upperValue}
+}
