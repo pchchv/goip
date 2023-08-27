ba0c1b0b191eb945eafa7bd8d2fc38760c778588
pchchv <ipchchv@gmail.com>
2023-08-27 19:40:09 +0300

    Add an IPv4AddressSegment type

---
+++ ipv4_segment.go
@@ -0,0 +1,12 @@
+package goip
+
+// IPv4AddressSegment represents a segment of an IPv4 address.
+// An IPv4 segment contains a single value or a range of sequential values,
+// a prefix length, and it has bit length of 8 bits.
+//
+// Like strings, segments are immutable, which also makes them concurrency-safe.
+//
+// See AddressSegment for more details regarding segments.
+type IPv4AddressSegment struct {
+	ipAddressSegmentInternal
+}
