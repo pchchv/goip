f2b692f4f2b44ba203f88e9b749713a1fca80e86
pchchv <ipchchv@gmail.com>
2023-08-28 13:58:38 +0300

    GetBlockMaskPrefixLen and checkForPrefixMask methods

--- ip_section.go
+++ ip_section.go
@@ -1,6 +1,10 @@
 package goip
 
-import "github.com/pchchv/goip/address_string"
+import (
+	"unsafe"
+
+	"github.com/pchchv/goip/address_string"
+)
 
 var (
 	rangeWildcard                 = new(address_string.WildcardsBuilder).ToWildcards()
@@ -29,6 +33,109 @@ func (section *ipAddressSectionInternal) GetNetworkPrefixLen() PrefixLen {
 	return section.getNetworkPrefixLen().copy()
 }
 
+// GetBlockMaskPrefixLen returns the prefix length if this address section is equivalent to the mask for a CIDR prefix block.
+// Otherwise, it returns nil.
+// A CIDR network mask is an address section with all ones in the network section and then all zeros in the host section.
+// A CIDR host mask is an address section with all zeros in the network section and then all ones in the host section.
+// The prefix length is the bit-length of the network section.
+//
+// Also, keep in mind that the prefix length returned by this method is not equivalent to the prefix length of this instance,
+// indicating the network and host section of this address section.
+// The prefix length returned here indicates the whether the value of this address can be used as a mask for the network and host
+// section of any other address.  Therefore the two values can be different values, or one can be nil while the other is not.
+//
+// This method applies only to the lower value of the range if this section represents multiple values.
+func (section *ipAddressSectionInternal) GetBlockMaskPrefixLen(network bool) PrefixLen {
+	cache := section.cache
+	if cache == nil {
+		return nil // no prefix
+	}
+	cachedMaskLens := (*maskLenSetting)(atomicLoadPointer((*unsafe.Pointer)(unsafe.Pointer(&cache.cachedMaskLens))))
+	if cachedMaskLens == nil {
+		networkMaskLen, hostMaskLen := section.checkForPrefixMask()
+		cachedMaskLens = &maskLenSetting{networkMaskLen, hostMaskLen}
+		dataLoc := (*unsafe.Pointer)(unsafe.Pointer(&cache.cachedMaskLens))
+		atomicStorePointer(dataLoc, unsafe.Pointer(cachedMaskLens))
+	}
+	if network {
+		return cachedMaskLens.networkMaskLen
+	}
+	return cachedMaskLens.hostMaskLen
+}
+
+func (section *ipAddressSectionInternal) checkForPrefixMask() (networkMaskLen, hostMaskLen PrefixLen) {
+	count := section.GetSegmentCount()
+	if count == 0 {
+		return
+	}
+	firstSeg := section.GetSegment(0)
+	checkingNetworkFront, checkingHostFront := true, true
+	var checkingNetworkBack, checkingHostBack bool
+	var prefixedSeg int
+	prefixedSegPrefixLen := BitCount(0)
+	maxVal := firstSeg.GetMaxValue()
+	for i := 0; i < count; i++ {
+		seg := section.GetSegment(i)
+		val := seg.GetSegmentValue()
+		if val == 0 {
+			if checkingNetworkFront {
+				prefixedSeg = i
+				checkingNetworkFront, checkingNetworkBack = false, true
+			} else if !checkingHostFront && !checkingNetworkBack {
+				return
+			}
+			checkingHostBack = false
+		} else if val == maxVal {
+			if checkingHostFront {
+				prefixedSeg = i
+				checkingHostFront, checkingHostBack = false, true
+			} else if !checkingHostBack && !checkingNetworkFront {
+				return
+			}
+			checkingNetworkBack = false
+		} else {
+			segNetworkMaskLen, segHostMaskLen := seg.checkForPrefixMask()
+			if segNetworkMaskLen != nil {
+				if checkingNetworkFront {
+					prefixedSegPrefixLen = segNetworkMaskLen.bitCount()
+					checkingNetworkBack = true
+					checkingHostBack = false
+					prefixedSeg = i
+				} else {
+					return
+				}
+			} else if segHostMaskLen != nil {
+				if checkingHostFront {
+					prefixedSegPrefixLen = segHostMaskLen.bitCount()
+					checkingHostBack = true
+					checkingNetworkBack = false
+					prefixedSeg = i
+				} else {
+					return
+				}
+			} else {
+				return
+			}
+			checkingNetworkFront, checkingHostFront = false, false
+		}
+	}
+	if checkingNetworkFront {
+		// all ones
+		networkMaskLen = cacheBitCount(section.GetBitCount())
+		hostMaskLen = cacheBitCount(0)
+	} else if checkingHostFront {
+		// all zeros
+		hostMaskLen = cacheBitCount(section.GetBitCount())
+		networkMaskLen = cacheBitCount(0)
+	} else if checkingNetworkBack {
+		// ending in zeros, network mask
+		networkMaskLen = getNetworkPrefixLen(firstSeg.GetBitCount(), prefixedSegPrefixLen, prefixedSeg)
+	} else if checkingHostBack {
+		// ending in ones, host mask
+		hostMaskLen = getNetworkPrefixLen(firstSeg.GetBitCount(), prefixedSegPrefixLen, prefixedSeg)
+	}
+	return
+}
 // IPAddressSection is the address section of an IP address containing a certain number of consecutive IP address segments.
 // It represents a sequence of individual address segments.
 // Each segment has the same bit length.
