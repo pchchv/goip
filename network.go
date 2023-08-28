ef1289614bf0221ed0573fba3efaf960ca7dca42
pchchv <ipchchv@gmail.com>
2023-08-28 14:05:49 +0300

    Add an IPAddressNetwork type

--- network.go
+++ network.go
@@ -3,3 +3,16 @@ package goip
 type addressNetwork interface {
 	getAddressCreator() parsedAddressCreator
 }
+
+// IPAddressNetwork represents a network of addresses of a single IP version providing a
+// collection of standard address components for that version,
+// such as masks and loopbacks.
+type IPAddressNetwork interface {
+	GetLoopback() *IPAddress
+	GetNetworkMask(prefixLength BitCount) *IPAddress
+	GetPrefixedNetworkMask(prefixLength BitCount) *IPAddress
+	GetHostMask(prefixLength BitCount) *IPAddress
+	GetPrefixedHostMask(prefixLength BitCount) *IPAddress
+	getIPAddressCreator() ipAddressCreator
+	addressNetwork
+}
