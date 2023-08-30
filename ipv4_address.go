8ef91430dfda894952b2347cb1085dfaee8e0e9d
pchchv <ipchchv@gmail.com>
2023-08-30 11:19:38 +0300

    Add a IPv4Address type

--- ipv4_address.go
+++ ipv4_address.go
@@ -15,3 +15,17 @@ const (
 	IPv4SegmentMaxChars       = 3
 	ipv4BitsToSegmentBitshift = 3
 )
+
+// IPv4Address is an IPv4 address, or a subnet of multiple IPv4 addresses.
+// An IPv4 address is composed of 4 1-byte segments and can optionally have an associated prefix length.
+// Each segment can represent a single value or a range of values.
+// The zero value is "0.0.0.0".
+//
+// To construct one from a string, use NewIPAddressString, then use the ToAddress or GetAddress method of [IPAddressString],
+// and then use ToIPv4 to get an IPv4Address, assuming the string had an IPv4 format.
+//
+// For other inputs, use one of the multiple constructor functions like NewIPv4Address.
+// You can also use one of the multiple constructors for [IPAddress] like NewIPAddress and then convert using ToIPv4.
+type IPv4Address struct {
+	ipAddressInternal
+}
