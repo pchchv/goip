a2eafde3c2ede0cba507262aace297168dc0d079
pchchv <ipchchv@gmail.com>
2023-08-30 11:22:17 +0300

    Add an IPv4SegInt and IPv4SegmentValueProvider types

--- ipv4_segment.go
+++ ipv4_segment.go
@@ -1,5 +1,9 @@
 package goip
 
+type IPv4SegInt = uint8
+
+type IPv4SegmentValueProvider func(segmentIndex int) IPv4SegInt
+
 // IPv4AddressSegment represents a segment of an IPv4 address.
 // An IPv4 segment contains a single value or a range of sequential values,
 // a prefix length, and it has bit length of 8 bits.
