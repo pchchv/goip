8fd7c7ccd8c4f3220de41511514d24cc6141cd09
pchchv <ipchchv@gmail.com>
2023-08-31 12:55:46 +0300

    Add an ipv4DivsBlock type

--- ipv4_segment.go
+++ ipv4_segment.go
@@ -19,6 +19,10 @@ type IPv4SegInt = uint8
 
 type IPv4SegmentValueProvider func(segmentIndex int) IPv4SegInt
 
+type ipv4DivsBlock struct {
+	block []ipv4SegmentValues
+}
+
 // IPv4AddressSegment represents a segment of an IPv4 address.
 // An IPv4 segment contains a single value or a range of sequential values,
 // a prefix length, and it has bit length of 8 bits.
