598b179f901f6d163e364ae7c3d32a13c1e5a675
pchchv <ipchchv@gmail.com>
2023-09-04 12:48:49 +0300

    Implement a ToIP and ToSectionBase methods

--- ipv4_section.go
+++ ipv4_section.go
@@ -6,6 +6,21 @@ type IPv4AddressSection struct {
 	ipAddressSectionInternal
 }
 
+// ToIP converts to an IPAddressSection, a polymorphic type usable with all IP address sections.
+//
+// ToIP can be called with a nil receiver, enabling you to chain this method with methods that might return a nil pointer.
+func (section *IPv4AddressSection) ToIP() *IPAddressSection {
+	return (*IPAddressSection)(section)
+}
+
+// ToSectionBase converts to an AddressSection, a polymorphic type usable with all address sections.
+// Afterwards, you can convert back with ToIPv4.
+//
+// ToSectionBase can be called with a nil receiver, enabling you to chain this method with methods that might return a nil pointer.
+func (section *IPv4AddressSection) ToSectionBase() *AddressSection {
+	return section.ToIP().ToSectionBase()
+}
+
 func createIPv4Section(segments []*AddressDivision) *IPv4AddressSection {
 	return &IPv4AddressSection{
 		ipAddressSectionInternal{
