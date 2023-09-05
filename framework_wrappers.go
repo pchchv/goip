90187fb57e8194d0138bc4573d6c506bc82d0999
pchchv <ipchchv@gmail.com>
2023-09-05 12:29:48 +0300

    Add a WrappedAddressSection type

--- framework_wrappers.go
+++ framework_wrappers.go
@@ -4,3 +4,8 @@ package goip
 type WrappedAddress struct {
 	*Address
 }
+
+// WrappedAddressSection is the implementation of ExtendedSegmentSeries for address sections.
+type WrappedAddressSection struct {
+	*AddressSection
+}
