e4fed4c3e55ab9f876841106ad828608e02cd5b4
pchchv <ipchchv@gmail.com>
2023-09-06 21:39:22 +0300

    Implement a ToIP and ToAddressBase methods

--- ipv6_address.go
+++ ipv6_address.go
@@ -96,6 +96,24 @@ func (addr *IPv6Address) ToPrefixBlockLen(prefLen BitCount) *IPv6Address {
 	return addr.init().toPrefixBlockLen(prefLen).ToIPv6()
 }
 
+// ToIP converts to an IPAddress, a polymorphic type usable with all IP addresses and subnets.
+//
+// ToIP can be called with a nil receiver, enabling you to chain this method with methods that might return a nil pointer.
+func (addr *IPv6Address) ToIP() *IPAddress {
+	if addr != nil {
+		addr = addr.init()
+	}
+	return (*IPAddress)(addr)
+}
+
+// ToAddressBase converts to an Address, a polymorphic type usable with all addresses and subnets.
+// Afterwards, you can convert back with ToIPv6.
+//
+// ToAddressBase can be called with a nil receiver, enabling you to chain this method with methods that might return a nil pointer.
+func (addr *IPv6Address) ToAddressBase() *Address {
+	return addr.ToIP().ToAddressBase()
+}
+
 func newIPv6Address(section *IPv6AddressSection) *IPv6Address {
 	return createAddress(section.ToSectionBase(), NoZone).ToIPv6()
 }
