96913a1b4e969d449b9adbe19184576e9ca70a2b
pchchv <ipchchv@gmail.com>
2023-09-14 10:47:49 +0300

    Implement an increment method

--- section_iterator.go
+++ section_iterator.go
@@ -47,3 +47,39 @@ func (it *multiSegmentsIterator) updateVariations(start int) {
 		nextSet[i] = variations[i].Next().ToDiv()
 	}
 }
+
+func (it *multiSegmentsIterator) increment() (res []*AddressDivision) {
+	var previousSegs []*AddressDivision
+	// the current set of segments already holds the next iteration,
+	// this searches for the set of segments to follow.
+	variations := it.variations
+	nextSet := it.nextSet
+
+	for j := it.networkSegmentIndex; j >= 0; j-- { // for regular iterators (not prefix block), networkSegmentIndex is last segment (count - 1)
+		for variations[j].HasNext() {
+			if previousSegs == nil {
+				previousSegs = cloneDivs(nextSet)
+			}
+			nextSet[j] = variations[j].Next().ToDiv()
+			it.updateVariations(j + 1)
+			excludeFunc := it.excludeFunc
+			if excludeFunc != nil && excludeFunc(nextSet) {
+				// try again, starting over
+				j = it.networkSegmentIndex
+			} else {
+				return previousSegs
+			}
+		}
+	}
+
+	it.done = true
+
+	if previousSegs == nil {
+		// never found set of candidate segments
+		return nextSet
+	}
+	// found a candidate to follow, but was rejected.
+	// nextSet has that rejected candidate,
+	// so we must return the set that was created prior to that.
+	return previousSegs
+}
