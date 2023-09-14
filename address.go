162504114d5d508da0c17a054a62f16c4c47d0a8
pchchv <ipchchv@gmail.com>
2023-09-15 02:49:31 +0300

    getMaxSegmentValue and setPrefixLen methods

--- address.go
+++ address.go
@@ -337,6 +337,14 @@ func (addr *addressInternal) GetBytesPerSegment() int {
 	return section.GetBytesPerSegment()
 }
 
+func (addr *addressInternal) getMaxSegmentValue() SegInt {
+	return addr.section.GetMaxSegmentValue()
+}
+
+func (addr *addressInternal) setPrefixLen(prefixLen BitCount) *Address {
+	return addr.checkIdentity(addr.section.setPrefixLen(prefixLen))
+}
+
 // Address represents a single address or a set of multiple addresses, such as an IP subnet or a set of MAC addresses.
 //
 // Addresses consist of a sequence of segments, each with the same bit-size.
