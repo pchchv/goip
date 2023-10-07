8d03ece6ac75e17170ff4d9aa1224eeda47d1c5c
pchchv <ipchchv@gmail.com>
2023-10-07 10:48:14 +0300

    Add a parsedHost type

--- parsed_host.go
+++ parsed_host.go
@@ -1,6 +1,9 @@
 package goip
 
-import "github.com/pchchv/goip/address_error"
+import (
+	"github.com/pchchv/goip/address_error"
+	"github.com/pchchv/goip/address_string_param"
+)
 
 type parsedHostCache struct {
 	normalizedLabels []string
@@ -12,4 +15,14 @@ type embeddedAddress struct {
 	isReverseDNS       bool
 	addressStringError address_error.AddressStringError
 	addressProvider    ipAddressProvider
+}
+
+type parsedHost struct {
+	*parsedHostCache
+	separatorIndices []int // can be nil
+	normalizedFlags  []bool
+	labelsQualifier  parsedHostIdentifierStringQualifier
+	embeddedAddress  embeddedAddress
+	originalStr      string
+	params           address_string_param.HostNameParams
 }
