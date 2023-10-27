0e286db405d46b37af1beecf03e8f5a8683fd4cc
pchchv <ipchchv@gmail.com>
2023-10-27 10:44:43 +0300

    NewIPv6AddressFromMACSection&NewIPv6AddressFromZonedMACSection&newIPv6AddressFromZonedMAC

--- ipv6_address.go
+++ ipv6_address.go
@@ -1495,3 +1495,41 @@ func newIPv6AddressFromMAC(prefixSection *IPv6AddressSection, suffix *MACAddress
 	res.isMult = suffix.isMultiple() || prefixSection.isMultipleTo(4)
 	return newIPv6AddressZoned(res, zone), nil
 }
+
+// NewIPv6AddressFromMACSection constructs an IPv6 address from a modified EUI-64 (Extended Unique Identifier)
+// MAC address section and an IPv6 address section network prefix.
+//
+// If the supplied MAC address section is an 8-byte EUI-64, then it must match the required EUI-64 format of "xx-xx-ff-fe-xx-xx"
+// with the "ff-fe" section in the middle.
+//
+// If the supplied MAC address section is a 6-byte MAC-48 or EUI-48, then the "ff-fe" pattern will be inserted when converting to IPv6.
+//
+// The constructor will toggle the MAC U/L (universal/local) bit as required with EUI-64.
+//
+// The IPv6 address section must be at least 8 bytes (4 segments) in length.
+//
+// Any prefix length in the MAC address is ignored, while a prefix length in the IPv6 address is preserved but only up to the first 4 segments.
+//
+// The error is either an AddressValueError for sections that are of insufficient segment count,
+// or IncompatibleAddressError when unable to join two MAC segments, at least one with ranged values, into an equivalent IPV6 segment range.
+func NewIPv6AddressFromMACSection(prefix *IPv6AddressSection, suffix *MACAddressSection) (*IPv6Address, address_error.AddressError) {
+	return newIPv6AddressFromZonedMAC(prefix, suffix, NoZone)
+}
+
+// NewIPv6AddressFromZonedMACSection constructs an IPv6 address from a modified EUI-64 (Extended Unique Identifier) MAC address section,
+// an IPv6 address section network prefix, and a zone.
+//
+// It is similar to NewIPv6AddressFromMACSection but also allows you to specify a zone.
+//
+// It is similar to NewIPv6AddressFromMAC, which can supply a zone with the IPv6Address argument.
+func NewIPv6AddressFromZonedMACSection(prefix *IPv6AddressSection, suffix *MACAddressSection, zone string) (*IPv6Address, address_error.AddressError) {
+	return newIPv6AddressFromZonedMAC(prefix, suffix, zone)
+}
+
+func newIPv6AddressFromZonedMAC(prefix *IPv6AddressSection, suffix *MACAddressSection, zone string) (*IPv6Address, address_error.AddressError) {
+	suffixSegCount := suffix.GetSegmentCount()
+	if prefix.GetSegmentCount() < 4 || (suffixSegCount != ExtendedUniqueIdentifier48SegmentCount && suffixSegCount != ExtendedUniqueIdentifier64SegmentCount) {
+		return nil, &addressValueError{addressError: addressError{key: "ipaddress.mac.error.not.eui.convertible"}}
+	}
+	return newIPv6AddressFromMAC(prefix, suffix, zone)
+}
