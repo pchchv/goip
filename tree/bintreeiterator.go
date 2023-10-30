ad226aba040a26b2972212a6d0097ac5476ce1e0
pchchv <ipchchv@gmail.com>
2023-10-30 12:24:18 +0300

    Add a nodeIteratorRem type

--- tree/bintreeiterator.go
+++ tree/bintreeiterator.go
@@ -8,3 +8,10 @@ type nodeIterator[E Key, V any] interface {
 	HasNext
 	Next() *binTreeNode[E, V]
 }
+
+type nodeIteratorRem[E Key, V any] interface {
+	nodeIterator[E, V]
+	// Remove removes the last iterated element from the underlying trie, and returns that element.
+	// If there is no such element, it returns nil.
+	Remove() *binTreeNode[E, V]
+}
