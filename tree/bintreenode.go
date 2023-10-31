6228ce21b3829b2fcf1b6c40aa09843e8c360159
pchchv <ipchchv@gmail.com>
2023-10-31 12:16:16 +0300

    Implement a flagsFromState function

--- tree/bintreenode.go
+++ tree/bintreenode.go
@@ -1007,3 +1007,32 @@ func NodeString[E Key, V any](node nodePrinter[E, V]) string {
 
 	return fmt.Sprint(nonAddedNodeCircle, " ", key, " = ", val)
 }
+
+func flagsFromState(state fmt.State, verb rune) string {
+	flags := "# +-0"
+	vals := make([]rune, 0, len(flags)+5) // %, flags, width, '.', precision, verb
+	vals = append(vals, '%')
+	for i := 0; i < len(flags); i++ {
+		b := flags[i]
+		if state.Flag(int(b)) {
+			vals = append(vals, rune(b))
+		}
+	}
+
+	width, widthOK := state.Width()
+	precision, precisionOK := state.Precision()
+	if widthOK || precisionOK {
+		var wpv string
+		if widthOK && precisionOK {
+			wpv = fmt.Sprintf("%d.%d%c", width, precision, verb)
+		} else if widthOK {
+			wpv = fmt.Sprintf("%d%c", width, verb)
+		} else {
+			wpv = fmt.Sprintf(".%d%c", precision, verb)
+		}
+		return string(vals) + wpv
+	}
+
+	vals = append(vals, verb)
+	return string(vals)
+}
