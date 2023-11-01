7071d257576298e9333ec5270cbd1b313638e5b9
pchchv <ipchchv@gmail.com>
2023-11-01 13:42:46 +0300

    Implement a String, format and printTree methods

--- tree/bintree.go
+++ tree/bintree.go
@@ -1,5 +1,10 @@
 package tree
 
+import (
+	"fmt"
+	"strings"
+)
+
 type binTree[E Key, V any] struct {
 	root *binTreeNode[E, V]
 }
@@ -47,4 +52,39 @@ func (tree *binTree[E, V]) IsEmpty() bool {
 // with or without the non-added keys.
 func (tree *binTree[E, V]) TreeString(withNonAddedKeys bool) string {
 	return tree.GetRoot().TreeString(withNonAddedKeys, true)
+}
+
+// String returns a visual representation of the tree with one node per line.
+func (tree *binTree[E, V]) String() string {
+	return tree.TreeString(true)
+}
+
+func (tree binTree[E, V]) format(state fmt.State, verb rune) {
+	switch verb {
+	case 's', 'v':
+		_, _ = state.Write([]byte(tree.String()))
+		return
+	}
+	// In default fmt handling (see printValue), we write all the fields of each struct inside curlies {}
+	// When a pointer is encountered, the pointer is printed unless the nesting depth is 0
+	// How that pointer is printed varies a lot depending on the verb and flags.
+	// So, in the case of unsupported flags, let's print { rootPointer } where rootPointer is printed according to the flags and verb.
+	s := flagsFromState(state, verb)
+	rootStr := fmt.Sprintf(s, binTreeNodePtr[E, V](tree.root))
+	bytes := make([]byte, len(rootStr)+2)
+	bytes[0] = '{'
+	shifted := bytes[1:]
+	copy(shifted, rootStr)
+	shifted[len(rootStr)] = '}'
+	_, _ = state.Write(bytes)
+}
+
+func (tree *binTree[E, V]) printTree(builder *strings.Builder, inds indents, withNonAddedKeys bool) {
+	if tree == nil {
+		builder.WriteString(inds.nodeIndent)
+		builder.WriteString(nilString())
+		builder.WriteByte('\n')
+	} else {
+		tree.GetRoot().printTree(builder, inds, withNonAddedKeys, true)
+	}
 }
