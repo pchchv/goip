86405043daba229f63641f605635ec8dad86c346
pchchv <ipchchv@gmail.com>
2023-11-01 13:33:37 +0300

    Implement a Len and bitCount methods

--- tree/bits.go
+++ tree/bits.go
@@ -12,3 +12,18 @@ type BitCount = int // using signed integers allows for easier arithmetic
 // A PrefixLen indicates the length of the prefix for an address, section, division grouping, segment, or division.
 // The zero value, which is nil, indicates that there is no prefix length.
 type PrefixLen = *PrefixBitCount
+
+// Len returns the length of the prefix.
+// If the receiver is nil, representing the absence of a prefix length, returns 0.
+// It will also return 0 if the receiver is a prefix with length of 0.
+// To distinguish the two, compare the receiver with nil.
+func (p *PrefixBitCount) Len() BitCount {
+	if p == nil {
+		return 0
+	}
+	return p.bitCount()
+}
+
+func (p *PrefixBitCount) bitCount() BitCount {
+	return BitCount(*p)
+}
