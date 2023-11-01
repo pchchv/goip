94e02c316e5374f7328916f4735897de6b784126
pchchv <ipchchv@gmail.com>
2023-11-01 13:44:40 +0300

    Add a BinTrie type

--- tree/trie.go
+++ tree/trie.go
@@ -1,3 +1,23 @@
 package tree
 
 type EmptyValueType struct{}
+
+// BinTrie is a binary trie.
+//
+// To use BinTrie, your keys implement TrieKey.
+//
+// All keys are either fixed, in which the key value does not change,
+// or comprising of a prefix in which an initial sequence of bits does not change, and the the remaining bits represent all bit values.
+// The length of the initial fixed sequence of bits is the prefix length.
+// The total bit length is the same for all keys.
+//
+// A key with a prefix is also known as a prefix block, and represents all bit sequences with the same prefix.
+//
+// The zero value for BinTrie is a binary trie ready for use.
+//
+// Each node can be associated with a value, making BinTrie an associative binary trie.
+// If you do not wish to associate values to nodes, then use the type EmptyValueType,
+// in which case the value will be ignored in functions that print node strings.
+type BinTrie[E TrieKey[E], V any] struct {
+	binTree[E, V]
+}
