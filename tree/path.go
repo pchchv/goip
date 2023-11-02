e1170034cd84f5f0b8376b86e6b923e63e68cd69
pchchv <ipchchv@gmail.com>
2023-11-02 13:39:11 +0300

    Implement an IsAdded, Size and String methods

--- tree/path.go
+++ tree/path.go
@@ -35,3 +35,54 @@ func (node *PathNode[E, V]) GetValue() (val V) {
 	}
 	return
 }
+
+// IsAdded returns whether the node was "added".
+// Some binary tree nodes are considered "added" and others are not.
+// Those nodes created for key elements added to the tree are "added" nodes.
+// Those that are not added are those nodes created to serve as junctions for the added nodes.
+// Only added elements contribute to the size of a tree.
+// When removing nodes, non-added nodes are removed automatically whenever they are no longer needed,
+// which is when an added node has less than two added sub-nodes.
+func (node *PathNode[E, V]) IsAdded() bool {
+	return node != nil && node.added
+}
+
+// Size returns the count of nodes added to
+// the sub-tree starting from this node as root and moving downwards to sub-nodes.
+// This is a constant-time operation since the size is maintained in each node.
+func (node *PathNode[E, V]) Size() (storedSize int) {
+	if node != nil {
+		storedSize = node.storedSize
+		if storedSize == sizeUnknown {
+			prev, next := node, node.next
+			for ; next != nil && next.storedSize == sizeUnknown; prev, next = next, next.next {
+			}
+			var nodeSize int
+			for {
+				if prev.IsAdded() {
+					nodeSize++
+				}
+				if next != nil {
+					nodeSize += next.storedSize
+				}
+				prev.storedSize = nodeSize
+				if prev == node {
+					break
+				}
+				prev = node.previous
+			}
+			storedSize = node.storedSize
+		}
+	}
+	return
+}
+
+// Returns a visual representation of this node including the key,
+// with an open circle indicating this node is not an added node,
+// a closed circle indicating this node is an added node.
+func (node *PathNode[E, V]) String() string {
+	if node == nil {
+		return NodeString[E, V](nil)
+	}
+	return NodeString[E, V](node)
+}
