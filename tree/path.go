7e77d7fcf4654bf428ace9e14fda8123b9e970db
pchchv <ipchchv@gmail.com>
2023-11-02 13:38:08 +0300

    Next, Previous, GetKey and GetValue methods

--- tree/path.go
+++ tree/path.go
@@ -9,3 +9,29 @@ type PathNode[E Key, V any] struct {
 	value      V   // only for associative trie nodes
 	storedSize int // the number of added nodes below this one, including this one if added
 }
+
+// Next returns the next node in the path.
+func (node *PathNode[E, V]) Next() *PathNode[E, V] {
+	return node.next
+}
+
+// Previous returns the previous node in the path.
+func (node *PathNode[E, V]) Previous() *PathNode[E, V] {
+	return node.previous
+}
+
+// GetKey gets the key used for placing the node in the tree.
+func (node *PathNode[E, V]) GetKey() (key E) {
+	if node != nil {
+		return node.item
+	}
+	return
+}
+
+// GetValue returns the value assigned to the node
+func (node *PathNode[E, V]) GetValue() (val V) {
+	if node != nil {
+		val = node.value
+	}
+	return
+}
