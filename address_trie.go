95b652152716f61c192df81e40ff34070e2c4dea
pchchv <ipchchv@gmail.com>
2023-11-06 14:15:05 +0300

    checkBlockOrAddress and mustBeBlockOrAddress funcs

---
+++ address_trie.go
@@ -0,0 +1,19 @@
+package goip
+
+import "github.com/pchchv/goip/address_error"
+
+// Ensures the address is either an individual address or a prefix block subnet.
+// Returns a normalized address which has no prefix length if it is a single address,
+// or has a prefix length matching the prefix block size if it is a prefix block.
+func checkBlockOrAddress[T TrieKeyConstraint[T]](addr T) (res T, err address_error.IncompatibleAddressError) {
+	return addr.toSinglePrefixBlockOrAddress()
+}
+
+// Ensures the address is either an individual address or a prefix block subnet.
+func mustBeBlockOrAddress[T TrieKeyConstraint[T]](addr T) T {
+	res, err := checkBlockOrAddress(addr)
+	if err != nil {
+		panic(err)
+	}
+	return res
+}
