a9beac5f793f5d292214aaf358aeb3151530304d
pchchv <ipchchv@gmail.com>
2023-11-09 11:22:54 +0300

    longestPrefixMatchNode&elementContains methods

--- address_trie.go
+++ address_trie.go
@@ -77,6 +77,17 @@ func (trie *trieBase[T, V]) longestPrefixMatch(addr T) (t T) {
 	return key.address
 }
 
+// only added nodes are added to the linked list.
+func (trie *trieBase[T, V]) longestPrefixMatchNode(addr T) *tree.BinTrieNode[trieKey[T], V] {
+	addr = mustBeBlockOrAddress(addr)
+	return trie.trie.LongestPrefixMatchNode(createKey(addr))
+}
+
+func (trie *trieBase[T, V]) elementContains(addr T) bool {
+	addr = mustBeBlockOrAddress(addr)
+	return trie.trie.ElementContains(createKey(addr))
+}
+
 // Trie is a compact binary trie (aka compact binary prefix tree, or binary radix trie), for addresses and/or CIDR prefix block subnets.
 // The prefixes in used by the prefix trie are the CIDR prefixes, or the full address in the case of individual addresses with no prefix length.
 // The elements of the trie are CIDR prefix blocks or addresses.
