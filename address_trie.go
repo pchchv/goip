03c1b70160fbb2a9e654bc9a59e68272685ebbb2
pchchv <ipchchv@gmail.com>
2023-11-09 11:34:55 +0300

    floorAddedNode&higherAddedNode methods

--- address_trie.go
+++ address_trie.go
@@ -169,6 +169,16 @@ func (trie *trieBase[T, V]) lowerAddedNode(addr T) *tree.BinTrieNode[trieKey[T],
 	return trie.trie.LowerAddedNode(createKey(addr))
 }
 
+func (trie *trieBase[T, V]) floorAddedNode(addr T) *tree.BinTrieNode[trieKey[T], V] {
+	addr = mustBeBlockOrAddress(addr)
+	return trie.trie.FloorAddedNode(createKey(addr))
+}
+
+func (trie *trieBase[T, V]) higherAddedNode(addr T) *tree.BinTrieNode[trieKey[T], V] {
+	addr = mustBeBlockOrAddress(addr)
+	return trie.trie.HigherAddedNode(createKey(addr))
+}
+
 // Trie is a compact binary trie (aka compact binary prefix tree, or binary radix trie), for addresses and/or CIDR prefix block subnets.
 // The prefixes in used by the prefix trie are the CIDR prefixes, or the full address in the case of individual addresses with no prefix length.
 // The elements of the trie are CIDR prefix blocks or addresses.
