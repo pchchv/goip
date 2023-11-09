c93180d0867b795c414146b4c4b5bfc4e24aabb6
pchchv <ipchchv@gmail.com>
2023-11-09 11:21:56 +0300

    removeElementsContainedBy&elementsContainedBy

--- address_trie.go
+++ address_trie.go
@@ -56,6 +56,16 @@ func (trie *trieBase[T, V]) remove(addr T) bool {
 	return trie.trie.Remove(createKey(addr))
 }
 
+func (trie *trieBase[T, V]) removeElementsContainedBy(addr T) *tree.BinTrieNode[trieKey[T], V] {
+	addr = mustBeBlockOrAddress(addr)
+	return trie.trie.RemoveElementsContainedBy(createKey(addr))
+}
+
+func (trie *trieBase[T, V]) elementsContainedBy(addr T) *tree.BinTrieNode[trieKey[T], V] {
+	addr = mustBeBlockOrAddress(addr)
+	return trie.trie.ElementsContainedBy(createKey(addr))
+}
+
 // Trie is a compact binary trie (aka compact binary prefix tree, or binary radix trie), for addresses and/or CIDR prefix block subnets.
 // The prefixes in used by the prefix trie are the CIDR prefixes, or the full address in the case of individual addresses with no prefix length.
 // The elements of the trie are CIDR prefix blocks or addresses.
