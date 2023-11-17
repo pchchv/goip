477723f8fa3256a4bc4f9d1d90b26e2e720c4bba
pchchv <ipchchv@gmail.com>
2023-11-17 14:42:01 +0300

    Add an IPv4AddressConverter type

--- converter.go
+++ converter.go
@@ -8,3 +8,13 @@ type IPv6AddressConverter interface {
 	// Otherwise, returns nil.
 	ToIPv6(address *IPAddress) *IPv6Address
 }
+
+// IPv4AddressConverter converts IP addresses to IPv4.
+type IPv4AddressConverter interface {
+	// ToIPv4 converts to IPv4.
+	// If the given address is IPv4,
+	// or can be converted to IPv4,
+	// returns that IPv4Address.
+	// Otherwise, returns nil.
+	ToIPv4(address *IPAddress) *IPv4Address
+}
