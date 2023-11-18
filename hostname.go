c366e47e55d31d131abe1439b0434c7d4536673b
pchchv <ipchchv@gmail.com>
2023-11-18 13:25:52 +0300

    Add a HostName type

--- hostname.go
+++ hostname.go
@@ -1,5 +1,7 @@
 package goip
 
+import "github.com/pchchv/goip/address_error"
+
 type resolveData struct {
 	resolvedAddrs []*IPAddress
 	err           error
@@ -8,4 +10,29 @@ type resolveData struct {
 type hostCache struct {
 	resolveData      *resolveData
 	normalizedString *string
+}
+
+// HostName represents an internet host name.  Can be a fully qualified domain name,
+// a simple host name, or an ip address string.
+// It can also include a port number or service name (which maps to a port).
+// It can include a prefix length or mask for either an ipaddress or host name string.
+// An IPv6 address can have an IPv6 zone.
+//
+// # Supported Formats
+//
+// You can use all host or address formats supported by nmap and all address formats supported by IPAddressString.
+// All manners of domain names are supported. When adding a prefix length or mask to a host name string,
+// it is to denote the subnet of the resolved address.
+//
+// Validation is done separately from DNS resolution to avoid unnecessary DNS lookups.
+//
+// See RFC 3513, RFC 2181, RFC 952, RFC 1035, RFC 1034, RFC 1123, RFC 5890 or the list of rfcs for IPAddress.
+// For IPv6 addresses in host, see RFC 2732 specifying "[]" notation
+// and RFC 3986 and RFC 4038 (combining IPv6 "[]" notation with prefix or zone)
+// and SMTP RFC 2821 for alternative uses of "[]" notation for both IPv4 and IPv6.
+type HostName struct {
+	str           string
+	parsedHost    *parsedHost
+	validateError address_error.HostNameError
+	*hostCache
 }
