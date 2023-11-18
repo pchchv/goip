da94c1bc5e742a627c953fc0ee87637f2e0cf341
pchchv <ipchchv@gmail.com>
2023-11-18 13:18:23 +0300

    Implement an incrementRange and add functions

--- increment.go
+++ increment.go
@@ -42,3 +42,57 @@ func checkOverflowBig(increment int64, bigIncrement, lowerValue, upperValue, cou
 	}
 	return false
 }
+
+// rangeIncrement the positive value of the number of increments through the range (0 means take lower or upper value in range)
+func incrementRange(section *AddressSection, increment int64, lowerProducer func() *AddressSection, prefixLength PrefixLen) *AddressSection {
+	if increment == 0 {
+		return lowerProducer()
+	}
+
+	segCount := section.GetSegmentCount()
+	newSegments := make([]*AddressDivision, segCount)
+	for i := segCount - 1; i >= 0; i-- {
+		seg := section.GetSegment(i)
+		segRange := seg.GetValueCount()
+		segRange64 := int64(segRange)
+		revolutions := increment / segRange64
+		remainder := increment % segRange64
+		val := seg.getSegmentValue() + SegInt(remainder)
+		segPrefixLength := getSegmentPrefixLength(section.GetBitsPerSegment(), prefixLength, i)
+		newSegment := createAddressDivision(seg.deriveNewMultiSeg(val, val, segPrefixLength))
+		newSegments[i] = newSegment
+		if revolutions == 0 {
+			for i--; i >= 0; i-- {
+				original := section.GetSegment(i)
+				val = original.getSegmentValue()
+				segPrefixLength = getSegmentPrefixLength(section.GetBitsPerSegment(), prefixLength, i)
+				newSegment = createAddressDivision(seg.deriveNewMultiSeg(val, val, segPrefixLength))
+				newSegments[i] = newSegment
+			}
+			break
+		} else {
+			increment = revolutions
+		}
+	}
+	return createSection(newSegments, prefixLength, section.getAddrType())
+}
+
+func add(section *AddressSection, fullValue uint64, increment int64, creator addressSegmentCreator, prefixLength PrefixLen) *AddressSection {
+	var val uint64
+	segCount := section.GetSegmentCount()
+	if increment < 0 {
+		val = fullValue - uint64(-increment)
+	} else {
+		val = fullValue + uint64(increment)
+	}
+
+	newSegs := createSegmentsUint64(
+		segCount,
+		0,
+		val,
+		section.GetBytesPerSegment(),
+		section.GetBitsPerSegment(),
+		creator,
+		prefixLength)
+	return createSection(newSegs, prefixLength, section.getAddrType())
+}
