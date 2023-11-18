8e5d540d6d906acf3d8300f19b61cd2648d084b6
pchchv <ipchchv@gmail.com>
2023-11-18 13:12:18 +0300

    NewFilteredIPAddrIterator&NewFilteredAddrIterator funcs

--- filtered_iterator.go
+++ filtered_iterator.go
@@ -43,3 +43,19 @@ func (it *filteredIPAddrIterator) Next() (res *IPAddress) {
 func (it *filteredIPAddrIterator) HasNext() bool {
 	return it.next != nil
 }
+
+// NewFilteredIPAddrIterator returns an iterator similar to the passed in iterator,
+// but skipping those elements for which the "skip" function returns true
+func NewFilteredIPAddrIterator(iter Iterator[*IPAddress], skip func(*IPAddress) bool) Iterator[*IPAddress] {
+	res := &filteredIPAddrIterator{skip: skip, iter: iter}
+	res.Next()
+	return res
+}
+
+// NewFilteredAddrIterator modifies an address iterator to skip certain addresses,
+// skipping those elements for which the "skip" function returns true
+func NewFilteredAddrIterator(iter Iterator[*Address], skip func(*Address) bool) Iterator[*Address] {
+	res := &filteredAddrIterator{skip: skip, iter: iter}
+	res.Next()
+	return res
+}
