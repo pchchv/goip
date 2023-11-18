43a0f3856a302268692b6e597cf468e8b4f08b0c
pchchv <ipchchv@gmail.com>
2023-11-18 12:55:45 +0300

    Implement an isDelimitedBoundary function

--- delimited_address_strings.go
+++ delimited_address_strings.go
@@ -1,3 +1,10 @@
 package goip
 
 type DelimitedAddressString string
+
+func isDelimitedBoundary(c byte) bool {
+	return c == IPv4SegmentSeparator ||
+		c == IPv6SegmentSeparator ||
+		c == RangeSeparator ||
+		c == MacDashedSegmentRangeSeparator
+}
