42ccb92ccdb53fdb168a472259f70b5b1b584bd1
pchchv <ipchchv@gmail.com>
2023-11-18 13:11:00 +0300

    Implement a Next and HasNext methods

--- filtered_iterator.go
+++ filtered_iterator.go
@@ -5,3 +5,19 @@ type filteredAddrIterator struct {
 	iter Iterator[*Address]
 	next *Address
 }
+
+func (it *filteredAddrIterator) Next() (res *Address) {
+	res = it.next
+	for {
+		next := it.iter.Next()
+		if next == nil || !it.skip(next) {
+			it.next = next
+			break
+		}
+	}
+	return res
+}
+
+func (it *filteredAddrIterator) HasNext() bool {
+	return it.next != nil
+}
