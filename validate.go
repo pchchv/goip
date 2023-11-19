021846a338ea30d5f0679ddbf502cc0514ddf0d3
pchchv <ipchchv@gmail.com>
2023-11-19 10:25:09 +0300

    isSingleSegmentIPv6&isSingleSegmentIPv4 funcs

--- validate.go
+++ validate.go
@@ -1,5 +1,29 @@
 package goip
 
+import (
+	"github.com/pchchv/goip/address_error"
+	"github.com/pchchv/goip/address_string_param"
+)
+
+const (
+	longSize                           = 64
+	longHexDigits                      = longSize >> 2
+	longBinaryDigits                   = longSize
+	maxWildcards                       = ipv6Base85SingleSegmentDigitCount - 1 // 20 wildcards is equivalent to a base 85 address
+	maxHostLength                      = 253
+	maxLabelLength                     = 63
+	maxHostSegments                    = 127
+	macSingleSegmentDigitCount         = 12
+	macDoubleSegmentDigitCount         = 6
+	macExtendedSingleSegmentDigitCount = 16
+	macExtendedDoubleSegmentDigitCount = 10
+	ipv6SingleSegmentDigitCount        = 32
+	ipv6BinarySingleSegmentDigitCount  = 128
+	ipv4BinarySingleSegmentDigitCount  = 32
+	ipv6Base85SingleSegmentDigitCount  = 20
+	ipv4SingleSegmentOctalDigitCount   = 11
+)
+
 var chars, extendedChars = createChars()
 
 type strValidator struct{}
@@ -30,5 +54,50 @@ func createChars() (chars [int('z') + 1]byte, extendedChars [int('~') + 1]byte) 
 		c := extendedDigits[i]
 		extendedChars[c] = i
 	}
+	return
+}
+
+func isSingleSegmentIPv6(str string, totalDigits int, isRange bool, frontTotalDigits int,
+	ipv6SpecificOptions address_string_param.IPv6AddressStringParams) (isSingle bool, err address_error.AddressStringError) {
+	backIsIpv6 := totalDigits == ipv6SingleSegmentDigitCount || // 32 hex chars with or without 0x
+		(ipv6SpecificOptions.AllowsBinary() && totalDigits == ipv6BinarySingleSegmentDigitCount+2) || // 128 binary chars with 0b
+		(isRange && totalDigits == 0 && (frontTotalDigits == ipv6SingleSegmentDigitCount ||
+			(ipv6SpecificOptions.AllowsBinary() && frontTotalDigits == ipv6BinarySingleSegmentDigitCount+2)))
+	if backIsIpv6 && isRange && totalDigits != 0 {
+		frontIsIpv6 := frontTotalDigits == ipv6SingleSegmentDigitCount ||
+			(ipv6SpecificOptions.AllowsBinary() && frontTotalDigits == ipv6BinarySingleSegmentDigitCount+2) ||
+			frontTotalDigits == 0
+		if !frontIsIpv6 {
+			err = &addressStringError{addressError{str: str, key: "ipaddress.error.too.few.segments.digit.count"}}
+			return
+		}
+	}
+	isSingle = backIsIpv6
+	return
+}
+
+// When checking for binary single segment, it is necessary to check the exact number of digits for IPv4.
+// This is because in IPv6 there is ambiguity between hexadecimal 32 characters starting with 0b and 0b before 30 binary characters.
+// Therefore, for IPv4, we must avoid 0b before 30 binary characters.
+// It is necessary to require 0b before 32 binary characters.
+// This only applies to single segment.
+// For segmented IPv4 there is no ambiguity, and we allow binary segments of different lengths as for inetAton.
+
+func isSingleSegmentIPv4(str string, nonZeroDigits, totalDigits int, isRange bool, frontNonZeroDigits, frontTotalDigits int,
+	ipv4SpecificOptions address_string_param.IPv4AddressStringParams) (isSingle bool, err address_error.AddressStringError) {
+	backIsIpv4 := nonZeroDigits <= ipv4SingleSegmentOctalDigitCount ||
+		(ipv4SpecificOptions.AllowsBinary() && totalDigits == ipv4BinarySingleSegmentDigitCount+2) ||
+		(isRange && totalDigits == 0 && (frontTotalDigits <= ipv4SingleSegmentOctalDigitCount ||
+			(ipv4SpecificOptions.AllowsBinary() && frontTotalDigits == ipv4BinarySingleSegmentDigitCount+2)))
+	if backIsIpv4 && isRange && totalDigits != 0 {
+		frontIsIpv4 := frontNonZeroDigits <= ipv4SingleSegmentOctalDigitCount ||
+			(ipv4SpecificOptions.AllowsBinary() && frontTotalDigits == ipv4BinarySingleSegmentDigitCount+2) ||
+			frontTotalDigits == 0
+		if !frontIsIpv4 {
+			err = &addressStringError{addressError{str: str, key: "ipaddress.error.too.few.segments.digit.count"}}
+			return
+		}
+	}
+	isSingle = backIsIpv4
 	return
 }
