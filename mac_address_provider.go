8b3c99bc095563a08cb62dff58d353bcf1205acd
pchchv <ipchchv@gmail.com>
2023-11-20 11:33:51 +0300

    Add a macAddressAllProvider type

--- mac_address_provider.go
+++ mac_address_provider.go
@@ -1,6 +1,8 @@
 package goip
 
 import (
+	"sync"
+
 	"github.com/pchchv/goip/address_error"
 	"github.com/pchchv/goip/address_string_param"
 )
@@ -8,4 +10,10 @@ import (
 type macAddressProvider interface {
 	getAddress() (*MACAddress, address_error.IncompatibleAddressError)
 	getParameters() address_string_param.MACAddressStringParams // parameters of the address created by parsing
+}
+
+type macAddressAllProvider struct {
+	validationOptions address_string_param.MACAddressStringParams
+	address           *MACAddress
+	creationLock      *sync.Mutex
 }
