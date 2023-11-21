64caa56dae8e4cde54cb3d8766703bc2dfc59d93
pchchv <ipchchv@gmail.com>
2023-11-21 15:03:37 +0300

    Implement an applyOperatorToLowerUpper function

--- split.go
+++ split.go
@@ -19,3 +19,41 @@ func checkPrefixBlockContainment(first, other ExtendedIPSegmentSeries) ExtendedI
 	}
 	return nil
 }
+
+func applyOperatorToLowerUpper(first, other ExtendedIPSegmentSeries, removePrefixes bool,
+	operatorFunctor func(lower, upper ExtendedIPSegmentSeries) []ExtendedIPSegmentSeries) []ExtendedIPSegmentSeries {
+	var lower, upper ExtendedIPSegmentSeries
+	if seriesValsSame(first, other) {
+		if removePrefixes && first.IsPrefixed() {
+			if other.IsPrefixed() {
+				lower = first.WithoutPrefixLen()
+			} else {
+				lower = other
+			}
+		} else {
+			lower = first
+		}
+		upper = lower.GetUpper()
+		lower = lower.GetLower()
+	} else {
+		firstLower := first.GetLower()
+		otherLower := other.GetLower()
+		firstUpper := first.GetUpper()
+		otherUpper := other.GetUpper()
+		if LowValueComparator.CompareSeries(firstLower, otherLower) > 0 {
+			lower = otherLower
+		} else {
+			lower = firstLower
+		}
+		if LowValueComparator.CompareSeries(firstUpper, otherUpper) < 0 {
+			upper = otherUpper
+		} else {
+			upper = firstUpper
+		}
+		if removePrefixes {
+			lower = lower.WithoutPrefixLen()
+			upper = upper.WithoutPrefixLen()
+		}
+	}
+	return operatorFunctor(lower, upper)
+}
