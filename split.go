6ef10b72e877edf216790b3147a4829d378c9573
pchchv <ipchchv@gmail.com>
2023-11-21 15:02:13 +0300

    checkPrefixBlockFormat&checkPrefixBlockContainment funcs

---
+++ split.go
@@ -0,0 +1,21 @@
+package goip
+
+func checkPrefixBlockFormat(container, contained ExtendedIPSegmentSeries, checkEqual bool) (result ExtendedIPSegmentSeries) {
+	if container.IsPrefixed() && container.IsSinglePrefixBlock() {
+		result = container
+	} else if checkEqual && contained.IsPrefixed() && container.CompareSize(contained) == 0 && contained.IsSinglePrefixBlock() {
+		result = contained
+	} else {
+		result = container.AssignPrefixForSingleBlock() // this returns nil if cannot be a prefix block
+	}
+	return
+}
+
+func checkPrefixBlockContainment(first, other ExtendedIPSegmentSeries) ExtendedIPSegmentSeries {
+	if first.Contains(other) {
+		return checkPrefixBlockFormat(first, other, true)
+	} else if other.Contains(first) {
+		return checkPrefixBlockFormat(other, first, false)
+	}
+	return nil
+}
