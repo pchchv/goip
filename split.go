f6d6bf961901b2ec4b59399d757c86b669a69560
pchchv <ipchchv@gmail.com>
2023-11-21 15:08:32 +0300

    Implement an init, push and pop methods

--- split.go
+++ split.go
@@ -6,6 +6,44 @@ type seriesStack struct {
 	bits        []BitCount
 }
 
+// init grows to have capacity at least as large as size.
+func (stack *seriesStack) init(size int) {
+	if stack.seriesPairs == nil {
+		stack.seriesPairs = make([]ExtendedIPSegmentSeries, 0, size<<1)
+		stack.indexes = make([]int, 0, size)
+		stack.bits = make([]BitCount, 0, size)
+	}
+}
+
+func (stack *seriesStack) push(lower, upper ExtendedIPSegmentSeries, previousSegmentBits BitCount, currentSegment int) {
+	stack.seriesPairs = append(stack.seriesPairs, lower, upper)
+	stack.indexes = append(stack.indexes, currentSegment)
+	stack.bits = append(stack.bits, previousSegmentBits)
+}
+
+func (stack *seriesStack) pop() (popped bool, lower, upper ExtendedIPSegmentSeries, previousSegmentBits BitCount, currentSegment int) {
+	seriesPairs := stack.seriesPairs
+	length := len(seriesPairs)
+	if length <= 0 {
+		return
+	}
+
+	length--
+	upper = seriesPairs[length]
+	length--
+	lower = seriesPairs[length]
+	stack.seriesPairs = seriesPairs[:length]
+	indexes := stack.indexes
+	length = len(indexes) - 1
+	currentSegment = indexes[length]
+	stack.indexes = indexes[:length]
+	stackbits := stack.bits
+	previousSegmentBits = stackbits[length]
+	stack.bits = stackbits[:length]
+	popped = true
+	return
+}
+
 func checkPrefixBlockFormat(container, contained ExtendedIPSegmentSeries, checkEqual bool) (result ExtendedIPSegmentSeries) {
 	if container.IsPrefixed() && container.IsSinglePrefixBlock() {
 		result = container
