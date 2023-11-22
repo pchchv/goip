75ccba416cff660eff91468127c0f96ed757904d
pchchv <ipchchv@gmail.com>
2023-11-22 15:47:18 +0300

    Implement a getMergedPrefixBlocks functions

--- merge.go
+++ merge.go
@@ -53,3 +53,160 @@ func organizeSequentially(sections []ExtendedIPSegmentSeries) (singleElement boo
 	})
 	return false, list
 }
+
+func getMergedPrefixBlocks(sections []ExtendedIPSegmentSeries) []ExtendedIPSegmentSeries {
+	singleElement, list := organizeSequentially(sections)
+	if singleElement {
+		return list
+	}
+
+	removedCount := 0
+	listLen := len(list)
+	first := sections[0]
+	bitCount := first.GetBitCount()
+	bitsPerSegment := first.GetBitsPerSegment()
+	bytesPerSegment := first.GetBytesPerSegment()
+	j := listLen - 1
+	i := j - 1
+top:
+	for j > 0 {
+		item := list[i]
+		otherItem := list[j]
+		compare := ReverseHighValueComparator.CompareSeries(item, otherItem)
+		// check for strict containment, case 1:
+		// w   z
+		//  x y
+
+		if compare > 0 {
+			removedCount++
+			k := j + 1
+			for k < listLen && list[k] == nil {
+				k++
+			}
+			if k < listLen {
+				list[j] = list[k]
+				list[k] = nil
+			} else {
+				list[j] = nil
+				j = i
+				i--
+			}
+			continue
+		}
+		// non-strict containment, case 2:
+		// w   z
+		// w   z
+		//
+		// reverse containment, case 3:
+		// w  y
+		// w   z
+		rcompare := ReverseLowValueComparator.CompareSeries(item, otherItem)
+		if rcompare >= 0 {
+			removedCount++
+			list[i] = otherItem
+			list[j] = nil
+			j = i
+			i--
+			continue
+		}
+		// check for merge, case 4:
+		// w   x
+		//      y   z
+		// where x and y adjacent, becoming:
+		// w        z
+		//
+		prefixLen := item.GetPrefixLen()
+		otherPrefixLen := otherItem.GetPrefixLen()
+		if !prefixLen.Equal(otherPrefixLen) {
+			j = i
+			i--
+			continue
+		}
+
+		var matchBitIndex BitCount
+		if prefixLen == nil {
+			matchBitIndex = bitCount - 1
+		} else {
+			matchBitIndex = prefixLen.bitCount() - 1
+		}
+
+		var lastMatchSegmentIndex, lastBitSegmentIndex int
+		if matchBitIndex != 0 {
+			lastMatchSegmentIndex = getNetworkSegmentIndex(matchBitIndex, bytesPerSegment, bitsPerSegment)
+			lastBitSegmentIndex = getHostSegmentIndex(matchBitIndex, bytesPerSegment, bitsPerSegment)
+		}
+
+		itemSegment := item.GetGenericSegment(lastMatchSegmentIndex)
+		otherItemSegment := otherItem.GetGenericSegment(lastMatchSegmentIndex)
+		itemSegmentValue := itemSegment.GetSegmentValue()
+		otherItemSegmentValue := otherItemSegment.GetSegmentValue()
+		segmentLastBitIndex := bitsPerSegment - 1
+		if lastBitSegmentIndex == lastMatchSegmentIndex {
+			segmentBitToCheck := matchBitIndex % bitsPerSegment
+			shift := segmentLastBitIndex - segmentBitToCheck
+			itemSegmentValue >>= uint(shift)
+			otherItemSegmentValue >>= uint(shift)
+		} else {
+			itemBitValue := item.GetGenericSegment(lastBitSegmentIndex).GetSegmentValue()
+			otherItemBitalue := otherItem.GetGenericSegment(lastBitSegmentIndex).GetSegmentValue()
+
+			// we will make space for the last bit so we can do a single comparison
+			itemSegmentValue = (itemSegmentValue << 1) | (itemBitValue >> uint(segmentLastBitIndex))
+			otherItemSegmentValue = (otherItemSegmentValue << 1) | (otherItemBitalue >> uint(segmentLastBitIndex))
+		}
+
+		if itemSegmentValue != otherItemSegmentValue {
+			itemSegmentValue ^= 1 // the ^ 1 flips the first bit
+			if itemSegmentValue != otherItemSegmentValue {
+				// neither an exact match nor a match when flipping the bit, so move on
+				j = i
+				i--
+				continue
+			} // else we will merge these two into a single prefix block, presuming the initial segments match
+		}
+
+		// check initial segments
+		for k := lastMatchSegmentIndex - 1; k >= 0; k-- {
+			itemSegment = item.GetGenericSegment(k)
+			otherItemSegment = otherItem.GetGenericSegment(k)
+			val := itemSegment.GetSegmentValue()
+			otherVal := otherItemSegment.GetSegmentValue()
+			if val != otherVal {
+				j = i
+				i--
+				continue top
+			}
+		}
+
+		joinedItem := otherItem.ToPrefixBlockLen(matchBitIndex)
+		list[i] = joinedItem
+		removedCount++
+		k := j + 1
+		for k < listLen && list[k] == nil {
+			k++
+		}
+
+		if k < listLen {
+			list[j] = list[k]
+			list[k] = nil
+		} else {
+			list[j] = nil
+			j = i
+			i--
+		}
+	}
+
+	if removedCount > 0 {
+		newSize := listLen - removedCount
+		for k, l := 0, 0; k < newSize; k, l = k+1, l+1 {
+			for list[l] == nil {
+				l++
+			}
+			if k != l {
+				list[k] = list[l]
+			}
+		}
+		list = list[:newSize]
+	}
+	return list
+}
