cb09430efbe0b478e51ca69c0a6c4181704a1c73
pchchv <ipchchv@gmail.com>
2023-11-24 22:18:15 +0300

    Implement a HasNext and Next methods

--- range_iterator.go
+++ range_iterator.go
@@ -7,3 +7,37 @@ type sequRangeIterator[T SequentialRangeConstraint[T]] struct {
 	prefixLength        BitCount
 	notFirst            bool
 }
+
+func (it *sequRangeIterator[T]) HasNext() bool {
+	return it.prefixBlockIterator.HasNext()
+}
+
+func (it *sequRangeIterator[T]) Next() (res *SequentialRange[T]) {
+	if it.HasNext() {
+		next := it.prefixBlockIterator.Next()
+		if !it.notFirst {
+			it.notFirst = true
+			// next is a prefix block
+			lower := it.rng.GetLower()
+			prefLen := it.prefixLength
+			if it.HasNext() {
+				if !lower.IncludesZeroHostLen(prefLen) {
+					return it.creator(lower, next.GetUpper())
+				}
+			} else {
+				upper := it.rng.GetUpper()
+				if !lower.IncludesZeroHostLen(prefLen) || !upper.IncludesMaxHostLen(prefLen) {
+					return it.creator(lower, upper)
+				}
+			}
+		} else if !it.HasNext() {
+			upper := it.rng.GetUpper()
+			if !upper.IncludesMaxHostLen(it.prefixLength) {
+				return it.creator(next.GetLower(), upper)
+			}
+		}
+		lower, upper := next.getLowestHighestAddrs()
+		return newSequRangeUnchecked(lower, upper, lower != upper)
+	}
+	return
+}
