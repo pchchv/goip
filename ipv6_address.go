bbe10e883caf1e89f5707b217e2324d36d23e094
pchchv <ipchchv@gmail.com>
2023-12-03 11:41:24 +0300

    Implement a ToHexString and ToOctalString methods

--- ipv6_address.go
+++ ipv6_address.go
@@ -1691,6 +1691,30 @@ func (addr *IPv6Address) MergeToPrefixBlocks(addrs ...*IPv6Address) []*IPv6Addre
 	return cloneToIPv6Addrs(blocks)
 }
 
+// ToHexString writes this address as a single hexadecimal value
+// (possibly two values if a range that is not a prefixed block),
+// the number of digits according to the bit count, with or without a preceding "0x" prefix.
+//
+// If a subnet cannot be written as a single prefix block or a range of two values, an error is returned.
+func (addr *IPv6Address) ToHexString(with0xPrefix bool) (string, address_error.IncompatibleAddressError) {
+	if addr == nil {
+		return nilString(), nil
+	}
+	return addr.init().toHexString(with0xPrefix)
+}
+
+// ToOctalString writes this address as a single octal value
+// (possibly two values if a range that is not a prefixed block),
+// the number of digits according to the bit count, with or without a preceding "0" prefix.
+//
+// If a subnet cannot be written as a single prefix block or a range of two values, an error is returned.
+func (addr *IPv6Address) ToOctalString(with0Prefix bool) (string, address_error.IncompatibleAddressError) {
+	if addr == nil {
+		return nilString(), nil
+	}
+	return addr.init().toOctalString(with0Prefix)
+}
+
 func newIPv6Address(section *IPv6AddressSection) *IPv6Address {
 	return createAddress(section.ToSectionBase(), NoZone).ToIPv6()
 }
