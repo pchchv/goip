641900bd44f7db88517b02ad78e4d75a9196e7af
pchchv <ipchchv@gmail.com>
2023-12-04 13:30:50 +0300

    ToHexString, ToOctalString and ToBinaryString methods

--- ipv4_section.go
+++ ipv4_section.go
@@ -965,6 +965,39 @@ func (section *IPv4AddressSection) MergeToPrefixBlocks(sections ...*IPv4AddressS
 	return cloneToIPv4Sections(blocks), nil
 }
 
+// ToHexString writes this address section as a single hexadecimal value (possibly two values if a range that is not a prefixed block),
+// the number of digits according to the bit count, with or without a preceding "0x" prefix.
+//
+// If a multiple-valued section cannot be written as a single prefix block or a range of two values, an error is returned.
+func (section *IPv4AddressSection) ToHexString(with0xPrefix bool) (string, address_error.IncompatibleAddressError) {
+	if section == nil {
+		return nilString(), nil
+	}
+	return section.toHexString(with0xPrefix)
+}
+
+// ToOctalString writes this address section as a single octal value (possibly two values if a range that is not a prefixed block),
+// the number of digits according to the bit count, with or without a preceding "0" prefix.
+//
+// If a multiple-valued section cannot be written as a single prefix block or a range of two values, an error is returned.
+func (section *IPv4AddressSection) ToOctalString(with0Prefix bool) (string, address_error.IncompatibleAddressError) {
+	if section == nil {
+		return nilString(), nil
+	}
+	return section.toOctalString(with0Prefix)
+}
+
+// ToBinaryString writes this address section as a single binary value (possibly two values if a range that is not a prefixed block),
+// the number of digits according to the bit count, with or without a preceding "0b" prefix.
+//
+// If a multiple-valued section cannot be written as a single prefix block or a range of two values, an error is returned.
+func (section *IPv4AddressSection) ToBinaryString(with0bPrefix bool) (string, address_error.IncompatibleAddressError) {
+	if section == nil {
+		return nilString(), nil
+	}
+	return section.toBinaryString(with0bPrefix)
+}
+
 // InetAtonRadix represents a radix for printing an address string.
 type InetAtonRadix int
 
