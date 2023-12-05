6ff38c670be45a94162c88b94bad5e04eb98c20f
pchchv <ipchchv@gmail.com>
2023-12-05 12:46:06 +0300

    ToHexString, ToOctalString and ToBinaryString methods

--- ipv4_address.go
+++ ipv4_address.go
@@ -1478,6 +1478,45 @@ func (addr *IPv4Address) TrieDecrement() *IPv4Address {
 	return nil
 }
 
+// ToHexString writes this address as a single hexadecimal value
+// (possibly two values if a range that is not a prefixed block),
+// the number of digits according to the bit count,
+// with or without a preceding "0x" prefix.
+//
+// If a subnet cannot be written as a single prefix block or a range of two values, an error is returned.
+func (addr *IPv4Address) ToHexString(with0xPrefix bool) (string, address_error.IncompatibleAddressError) {
+	if addr == nil {
+		return nilString(), nil
+	}
+	return addr.init().toHexString(with0xPrefix)
+}
+
+// ToOctalString writes this address as a single octal value
+// (possibly two values if a range that is not a prefixed block),
+// the number of digits according to the bit count,
+// with or without a preceding "0" prefix.
+//
+// If a subnet cannot be written as a single prefix block or a range of two values, an error is returned.
+func (addr *IPv4Address) ToOctalString(with0Prefix bool) (string, address_error.IncompatibleAddressError) {
+	if addr == nil {
+		return nilString(), nil
+	}
+	return addr.init().toOctalString(with0Prefix)
+}
+
+// ToBinaryString writes this address as a single binary value
+// (possibly two values if a range that is not a prefixed block),
+// the number of digits according to the bit count,
+// with or without a preceding "0b" prefix.
+//
+// If a subnet cannot be written as a single prefix block or a range of two values, an error is returned.
+func (addr *IPv4Address) ToBinaryString(with0bPrefix bool) (string, address_error.IncompatibleAddressError) {
+	if addr == nil {
+		return nilString(), nil
+	}
+	return addr.init().toBinaryString(with0bPrefix)
+}
+
 func newIPv4Address(section *IPv4AddressSection) *IPv4Address {
 	return createAddress(section.ToSectionBase(), NoZone).ToIPv4()
 }
