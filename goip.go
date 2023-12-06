6e0bc7d64a7151db2e26d60b6424d099c3e78f70
pchchv <ipchchv@gmail.com>
2023-12-06 11:54:16 +0300

    IncrementBoundary, Increment and Intersect methods

--- goip.go
+++ goip.go
@@ -1196,6 +1196,57 @@ func (addr *IPAddress) PrefixBlockIterator() Iterator[*IPAddress] {
 	return ipAddrIterator{addr.init().prefixIterator(true)}
 }
 
+// IncrementBoundary returns the address that is the given increment from the range boundaries of this subnet.
+//
+// If the given increment is positive, adds the value to the upper address (GetUpper) in the subnet range to produce a new address.
+// If the given increment is negative, adds the value to the lower address (GetLower) in the subnet range to produce a new address.
+// If the increment is zero, returns this address.
+//
+// If this is a single address value, that address is simply incremented by the given increment value, positive or negative.
+//
+// On address overflow or underflow, IncrementBoundary returns nil.
+func (addr *IPAddress) IncrementBoundary(increment int64) *IPAddress {
+	return addr.init().incrementBoundary(increment).ToIP()
+}
+
+// Increment returns the address from the subnet that is the given increment upwards into the subnet range,
+// with the increment of 0 returning the first address in the range.
+//
+// If the increment i matches or exceeds the subnet size count c, then i - c + 1
+// is added to the upper address of the range.
+// An increment matching the subnet count gives you the address just above the highest address in the subnet.
+//
+// If the increment is negative, it is added to the lower address of the range.
+// To get the address just below the lowest address of the subnet, use the increment -1.
+//
+// If this is just a single address value, the address is simply incremented by the given increment, positive or negative.
+//
+// If this is a subnet with multiple values, a positive increment i is equivalent i + 1 values from the subnet iterator and beyond.
+// For instance, a increment of 0 is the first value from the iterator, an increment of 1 is the second value from the iterator, and so on.
+// An increment of a negative value added to the subnet count is equivalent to the same number of iterator values preceding the upper bound of the iterator.
+// For instance, an increment of count - 1 is the last value from the iterator, an increment of count - 2 is the second last value, and so on.
+//
+// On address overflow or underflow, Increment returns nil.
+func (addr *IPAddress) Increment(increment int64) *IPAddress {
+	return addr.init().increment(increment).ToIP()
+}
+
+// Intersect returns the subnet whose addresses are found in both this and the given subnet argument, or nil if no such addresses exist.
+//
+// This is also known as the conjunction of the two sets of addresses.
+func (addr *IPAddress) Intersect(other *IPAddress) *IPAddress {
+	if thisAddr := addr.ToIPv4(); thisAddr != nil {
+		if oth := other.ToIPv4(); oth != nil {
+			return thisAddr.Intersect(oth).ToIP()
+		}
+	} else if thisAddr := addr.ToIPv6(); thisAddr != nil {
+		if oth := other.ToIPv6(); oth != nil {
+			return thisAddr.Intersect(oth).ToIP()
+		}
+	}
+	return nil
+}
+
 // IPVersion is the version type used by IP address types.
 type IPVersion int
 
